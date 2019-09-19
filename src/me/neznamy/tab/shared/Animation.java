f0788d700e1cc1aebb7c94db4d574455d79833dc
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2019-09-19 13:58:50 +0200

    fixed wrong number displayed

--- src/me/neznamy/tab/shared/Animation.java
+++ src/me/neznamy/tab/shared/Animation.java
@@ -14,9 +14,11 @@ public class Animation {
 			interval = 1000;
 		}
 		if (interval % 50 > 0) {
-			Shared.startupWarn("Animation \"" + name + "\" has a refresh interval of §e" + interval + "ms§c which is not divisible by 50! Animations can't refresh faster than every tick ( = 50 milliseconds). §bUsing " + interval + " for now.");
+			int oldInterval = interval;
 			interval -= interval%50;
 			if (interval == 0) interval = 50;
+			Shared.startupWarn("Animation \"" + name + "\" has a refresh interval of §e" + oldInterval + "ms§c which is not divisible by 50! Animations can't refresh faster than every tick ( = 50 milliseconds). §bUsing " + interval + " for now.");
+			
 		}
 		this.name = name;
 		this.messages = list.toArray(new String[0]);
