46c1cd31374a82e2dd52a3a6c6e5c2c1b5e346fa
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2019-12-13 16:42:17 +0100

    fixes and sync with bukkit main class

--- src/me/neznamy/tab/platforms/bungee/Main.java
+++ src/me/neznamy/tab/platforms/bungee/Main.java
@@ -12,29 +12,20 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.*;
 import me.neznamy.tab.premium.ScoreboardManager;
 import me.neznamy.tab.shared.*;
-import me.neznamy.tab.shared.Shared.CPUSample;
+import me.neznamy.tab.shared.BossBar;
 import me.neznamy.tab.shared.TabObjective.TabObjectiveType;
 import me.neznamy.tab.shared.packets.PacketPlayOutPlayerInfo;
 import me.neznamy.tab.shared.packets.UniversalPacketPlayOut;
 import me.neznamy.tab.shared.packets.PacketPlayOutPlayerInfo.EnumPlayerInfoAction;
-import me.neznamy.tab.shared.placeholders.Constant;
-import me.neznamy.tab.shared.placeholders.Placeholders;
-import me.neznamy.tab.shared.placeholders.PlayerPlaceholder;
-import me.neznamy.tab.shared.placeholders.ServerPlaceholder;
+import me.neznamy.tab.shared.placeholders.*;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
-import net.md_5.bungee.api.event.ChatEvent;
-import net.md_5.bungee.api.event.PlayerDisconnectEvent;
-import net.md_5.bungee.api.event.ServerSwitchEvent;
-import net.md_5.bungee.api.plugin.Command;
-import net.md_5.bungee.api.plugin.Listener;
-import net.md_5.bungee.api.plugin.Plugin;
-import net.md_5.bungee.event.EventHandler;
-import net.md_5.bungee.event.EventPriority;
-import net.md_5.bungee.protocol.packet.PlayerListItem;
-import net.md_5.bungee.protocol.packet.Team;
+import net.md_5.bungee.api.event.*;
+import net.md_5.bungee.api.plugin.*;
+import net.md_5.bungee.event.*;
+import net.md_5.bungee.protocol.packet.*;
 
 public class Main extends Plugin implements Listener, MainClass{
 
@@ -66,10 +57,9 @@ public class Main extends Plugin implements Listener, MainClass{
 	}
 	public void load(boolean broadcastTime, boolean inject) {
 		try {
-			Shared.disabled = false;
 			long time = System.currentTimeMillis();
+			Shared.disabled = false;
 			Shared.startupWarns = 0;
-			Shared.cpuHistory = new ArrayList<CPUSample>();
 			registerPlaceholders();
 			Configs.loadFiles();
 			Shared.registerAnimationPlaceholders();
@@ -129,10 +119,11 @@ public class Main extends Plugin implements Listener, MainClass{
 				inject(p.getUniqueId());
 				Placeholders.recalculateOnlineVersions();
 				HeaderFooter.playerJoin(p);
-				TabObjective.playerJoin(p);
 				BossBar.playerJoin(p);
+				TabObjective.playerJoin(p);
 				ScoreboardManager.register(p);
 				NameTag16.playerJoin(p);
+				BelowName.playerJoin(p);
 			} else {
 				String from = p.getWorldName();
 				String to = p.world = e.getPlayer().getServer().getInfo().getName();
@@ -174,15 +165,17 @@ public class Main extends Plugin implements Listener, MainClass{
 						Playerlist.modifyPacket(p, player);
 						packet = p.toBungee(null);
 					}
+					if (packet instanceof Team && NameTag16.enable) {
+						if (killPacket((Team)packet)) return;
+					}
 					if (packet instanceof ByteBuf && NameTag16.enable) {
-						ByteBuf buf = (ByteBuf) packet;
+						ByteBuf buf = ((ByteBuf) packet).duplicate();
 						byte packetId = buf.readByte();
 						Team team = null;
 						if (packetId == player.getVersion().getPacketPlayOutScoreboardTeamId()) {
 							team = new Team();
 							team.read(buf, null, player.getVersion().getNetworkId());
 						}
-						buf.resetReaderIndex();
 						if (team != null && killPacket(team)) return;
 					}
 				} catch (Throwable e){
@@ -192,9 +185,9 @@ public class Main extends Plugin implements Listener, MainClass{
 			}
 		});
 	}
-	public boolean killPacket(Object packetPlayOutScoreboardTeam){
-		if (((Team) packetPlayOutScoreboardTeam).getFriendlyFire() != 69) {
-			String[] players = ((Team) packetPlayOutScoreboardTeam).getPlayers();
+	public boolean killPacket(Team packet){
+		if (packet.getFriendlyFire() != 69) {
+			String[] players = packet.getPlayers();
 			if (players == null) return false;
 			for (ITabPlayer p : Shared.getPlayers()) {
 				for (String player : players) {
