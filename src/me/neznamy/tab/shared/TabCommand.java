ffec97e0e0a8122e1016c9bd31aa2a24f685fb7d
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2019-12-17 21:17:42 +0100

    better debug output

--- src/me/neznamy/tab/shared/TabCommand.java
+++ src/me/neznamy/tab/shared/TabCommand.java
@@ -257,7 +257,7 @@ public class TabCommand{
 	}
 	public static void sendMessage(ITabPlayer sender, String message) {
 		if (sender != null) {
-			sender.sendMessage(message.replace('&', Shared.COLOR));
+			sender.sendMessage(message);
 		} else {
 			Shared.mainClass.sendConsoleMessage(message);
 		}
@@ -287,7 +287,7 @@ public class TabCommand{
 					sortingType = "Tabprefix";
 				} else {
 					if (Configs.sortByPermissions) {
-						sortingType = "Permissions (ENABLED BY USER, DISABLED BY DEFAULT!)";
+						sortingType = "Permissions &c(this option was enabled by user, it is disabled by default!)";
 					} else {
 						sortingType = "Groups";
 					}
@@ -306,20 +306,43 @@ public class TabCommand{
 				sendMessage(sender, "&eFull permission group list: &a" + Arrays.toString(analyzed.getGroupsFromPermPlugin()));
 				sendMessage(sender, "&eChosen group: &a" + analyzed.getGroup());
 			}
-			if (sorting) sendMessage(sender, "&eTeam name: &a" +analyzed.getTeamName().replace("&", "&"));
+			if (sorting) {
+				if (analyzed.disabledNametag) {
+					sendMessage(sender, "&eTeam name: &cSorting disabled in player's world");
+				} else {
+					sendMessage(sender, "&eTeam name: &a" + analyzed.getTeamName());
+				}
+			}
 			if (Playerlist.enable) {
-				sendMessage(sender, "&9tabprefix: &b" + analyzed.properties.get("tabprefix").getCurrentRawValue() + " &7(static=" + analyzed.properties.get("tabprefix").isStatic() + ")");
-				sendMessage(sender, "&9tabsuffix: &b" + analyzed.properties.get("tabsuffix").getCurrentRawValue() + " &7(static=" + analyzed.properties.get("tabsuffix").isStatic() + ")");
-				sendMessage(sender, "&9tabname: &b" + analyzed.properties.get("customtabname").getCurrentRawValue() + " &7(static=" + analyzed.properties.get("customtabname").isStatic() + ")");
+				if (analyzed.disabledTablistNames) {
+					sendMessage(sender, "&9tabprefix: &cDisabled in player's world");
+					sendMessage(sender, "&9tabsuffix: &cDisabled in player's world");
+					sendMessage(sender, "&9tabname: &cDisabled in player's world");
+				} else {
+					sendMessage(sender, "&9tabprefix: &b" + analyzed.properties.get("tabprefix").getCurrentRawValue() + " &7(static=" + analyzed.properties.get("tabprefix").isStatic() + ")");
+					sendMessage(sender, "&9tabsuffix: &b" + analyzed.properties.get("tabsuffix").getCurrentRawValue() + " &7(static=" + analyzed.properties.get("tabsuffix").isStatic() + ")");
+					sendMessage(sender, "&9tabname: &b" + analyzed.properties.get("customtabname").getCurrentRawValue() + " &7(static=" + analyzed.properties.get("customtabname").isStatic() + ")");
+				}
 			}
 			if (NameTag16.enable || Configs.unlimitedTags) {
-				sendMessage(sender, "&9tagprefix: &b" + analyzed.properties.get("tagprefix").getCurrentRawValue() + " &7(static=" + analyzed.properties.get("tagprefix").isStatic() + ")");
-				sendMessage(sender, "&9tagsuffix: &b" + analyzed.properties.get("tagsuffix").getCurrentRawValue() + " &7(static=" + analyzed.properties.get("tagsuffix").isStatic() + ")");
+				if (analyzed.disabledNametag) {
+					sendMessage(sender, "&9tagprefix: &cDisabled in player's world");
+					sendMessage(sender, "&9tagsuffix: &cDisabled in player's world");
+				} else {
+					sendMessage(sender, "&9tagprefix: &b" + analyzed.properties.get("tagprefix").getCurrentRawValue() + " &7(static=" + analyzed.properties.get("tagprefix").isStatic() + ")");
+					sendMessage(sender, "&9tagsuffix: &b" + analyzed.properties.get("tagsuffix").getCurrentRawValue() + " &7(static=" + analyzed.properties.get("tagsuffix").isStatic() + ")");
+				}
 			}
 			if (Configs.unlimitedTags) {
-				sendMessage(sender, "&9abovename: &b" + analyzed.properties.get("abovename").getCurrentRawValue() + " &7(static=" + analyzed.properties.get("abovename").isStatic() + ")");
-				sendMessage(sender, "&9belowname: &b" + analyzed.properties.get("belowname").getCurrentRawValue() + " &7(static=" + analyzed.properties.get("belowname").isStatic() + ")");
-				sendMessage(sender, "&9tagname: &b" + analyzed.properties.get("customtagname").getCurrentRawValue() + " &7(static=" + analyzed.properties.get("customtagname").isStatic() + ")");
+				if (analyzed.disabledNametag) {
+					sendMessage(sender, "&9abovename: &cDisabled in player's world");
+					sendMessage(sender, "&9belowname: &cDisabled in player's world");
+					sendMessage(sender, "&9tagname: &cDisabled in player's world");
+				} else {
+					sendMessage(sender, "&9abovename: &b" + analyzed.properties.get("abovename").getCurrentRawValue() + " &7(static=" + analyzed.properties.get("abovename").isStatic() + ")");
+					sendMessage(sender, "&9belowname: &b" + analyzed.properties.get("belowname").getCurrentRawValue() + " &7(static=" + analyzed.properties.get("belowname").isStatic() + ")");
+					sendMessage(sender, "&9tagname: &b" + analyzed.properties.get("customtagname").getCurrentRawValue() + " &7(static=" + analyzed.properties.get("customtagname").isStatic() + ")");
+				}
 			}
 		}
 	}
