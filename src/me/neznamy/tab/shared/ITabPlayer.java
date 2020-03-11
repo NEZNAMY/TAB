c77acbe0ff42f97699bbd9de2c260aff4e0eecea
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2020-03-11 23:54:06 +0100

    wasn't as good idea as i originally thought

--- src/me/neznamy/tab/shared/ITabPlayer.java
+++ src/me/neznamy/tab/shared/ITabPlayer.java
@@ -17,7 +17,6 @@ import me.neznamy.tab.shared.features.BossBar.BossBarLine;
 import me.neznamy.tab.shared.packets.PacketPlayOutPlayerInfo.EnumGamemode;
 import me.neznamy.tab.shared.packets.PacketPlayOutPlayerInfo.EnumPlayerInfoAction;
 import me.neznamy.tab.shared.packets.PacketPlayOutPlayerInfo.PlayerInfoData;
-import me.neznamy.tab.shared.packets.PacketPlayOutPlayerListHeaderFooter;
 import me.neznamy.tab.shared.placeholders.Placeholder;
 import me.neznamy.tab.shared.placeholders.Placeholders;
 import me.neznamy.tab.shared.packets.PacketPlayOutPlayerInfo;
@@ -516,14 +515,7 @@ public abstract class ITabPlayer {
 	}
 
 	public void sendCustomPacket(UniversalPacketPlayOut packet) {
-		sendCustomPacket(packet, false);
-	}
-	public void sendCustomPacket(UniversalPacketPlayOut packet, boolean forceThroughDisabled) {
 		try {
-			if (packet instanceof PacketPlayOutPlayerListHeaderFooter) {
-				if (version.getMinorVersion() < ProtocolVersion.v1_8.getMinorVersion()) return;
-				if (disabledHeaderFooter && !forceThroughDisabled) return;
-			}
 			sendPacket(PacketAPI.buildPacket(packet, version));
 		} catch (Throwable e) {
 			Shared.errorManager.printError("An error occurred when creating " + packet.getClass().getSimpleName(), e);
--- src/me/neznamy/tab/shared/features/HeaderFooter.java
+++ src/me/neznamy/tab/shared/features/HeaderFooter.java
@@ -3,6 +3,7 @@ package me.neznamy.tab.shared.features;
 import me.neznamy.tab.shared.Configs;
 import me.neznamy.tab.shared.ITabPlayer;
 import me.neznamy.tab.shared.Property;
+import me.neznamy.tab.shared.ProtocolVersion;
 import me.neznamy.tab.shared.Shared;
 import me.neznamy.tab.shared.packets.PacketPlayOutPlayerListHeaderFooter;
 
@@ -22,7 +23,10 @@ public class HeaderFooter implements SimpleFeature{
 	}
 	@Override
 	public void unload() {
-		Shared.getPlayers().forEach(p -> p.sendCustomPacket(new PacketPlayOutPlayerListHeaderFooter("","")));
+		for (ITabPlayer p : Shared.getPlayers()) {
+			if (p.disabledHeaderFooter || p.getVersion().getMinorVersion() < ProtocolVersion.v1_8.getMinorVersion()) continue;
+			p.sendCustomPacket(new PacketPlayOutPlayerListHeaderFooter("",""));
+		}
 	}
 	@Override
 	public void onJoin(ITabPlayer p) {
@@ -35,12 +39,13 @@ public class HeaderFooter implements SimpleFeature{
 	public void onWorldChange(ITabPlayer p, String from, String to) {
 		if (p.disabledHeaderFooter) {
 			if (!p.isDisabledWorld(Configs.disabledHeaderFooter, from))
-				p.sendCustomPacket(new PacketPlayOutPlayerListHeaderFooter("", ""), true);
+				p.sendCustomPacket(new PacketPlayOutPlayerListHeaderFooter("", ""));
 		} else {
 			refreshHeaderFooter(p, true);
 		}
 	}
 	public void refreshHeaderFooter(ITabPlayer p, boolean force) {
+		if (p.disabledHeaderFooter || p.getVersion().getMinorVersion() < ProtocolVersion.v1_8.getMinorVersion()) return;
 		Property headerp = p.properties.get("header");
 		Property footerp = p.properties.get("footer");
 		boolean header = headerp.isUpdateNeeded();
