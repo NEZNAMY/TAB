7fa7e1c385a2962a5ba32ba84bfb863be08f390d
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2020-03-30 17:15:57 +0200

    showing plugin info to admins only on premium mode

--- src/me/neznamy/tab/shared/Shared.java
+++ src/me/neznamy/tab/shared/Shared.java
@@ -63,7 +63,7 @@ public class Shared {
 		if (Configs.SECRET_debugMode) mainClass.sendConsoleMessage("&7[TAB DEBUG] " + message);
 	}
 	public static void sendPluginInfo(ITabPlayer to) {
-		if (Premium.is()) return;
+		if (Premium.is() && !to.hasPermission("tab.admin")) return;
 		IChatBaseComponent message = new IChatBaseComponent("TAB v" + pluginVersion).setColor(EnumChatFormat.DARK_AQUA).onHoverShowText(Placeholders.colorChar + "aClick to visit plugin's spigot page").onClickOpenUrl("https://www.spigotmc.org/resources/57806/");
 		message.addExtra(new IChatBaseComponent(" by _NEZNAMY_").setColor(EnumChatFormat.BLACK));
 		to.sendCustomPacket(new PacketPlayOutChat(message));
