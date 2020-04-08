5f67f497c557d577512c9c95dcae7b1a0a583e96
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2020-04-08 23:20:40 +0200

    fuck, too late

--- src/me/neznamy/tab/shared/features/UpdateChecker.java
+++ src/me/neznamy/tab/shared/features/UpdateChecker.java
@@ -10,7 +10,7 @@ import me.neznamy.tab.shared.Shared;
 
 public class UpdateChecker {
 
-	private static final int currentVersionId = 272;
+	private static final int currentVersionId = 273;
 	
 	public UpdateChecker() {
 		new Thread(new Runnable() {
