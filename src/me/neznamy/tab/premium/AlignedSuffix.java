6725269ded54a60787b0455ff0acb772a1c431a4
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2020-04-19 14:15:39 +0200

    added more characters

--- src/me/neznamy/tab/premium/AlignedSuffix.java
+++ src/me/neznamy/tab/premium/AlignedSuffix.java
@@ -124,6 +124,9 @@ public class AlignedSuffix implements SimpleFeature{
 		widths.put((char)12304, 8);
 		widths.put((char)12305, 8);
 		widths.put((char)10084, 7);
+		widths.put((char)10004, 8);
+		widths.put('#', 5);
+		widths.put('!', 1);
 	}
 	public String fixTextWidth(ITabPlayer player, String prefixAndName, String suffix) {
 		int playerNameWidth = getTextWidth(IChatBaseComponent.fromColoredText(prefixAndName + suffix));
