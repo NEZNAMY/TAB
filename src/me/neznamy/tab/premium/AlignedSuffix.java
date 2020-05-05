d9efa852413b1a3b34cc7decdde1413ac1c130b1
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2020-05-05 21:35:48 +0200

    added more characters and better organization

--- src/me/neznamy/tab/premium/AlignedSuffix.java
+++ src/me/neznamy/tab/premium/AlignedSuffix.java
@@ -16,6 +16,46 @@ public class AlignedSuffix implements SimpleFeature{
 	public static final Map<Character, Integer> widths = new HashMap<Character, Integer>();
 
 	public AlignedSuffix() {
+		//32-47
+		widths.put(' ', 3);
+		widths.put('!', 1);
+		widths.put('"', 5);
+		widths.put('#', 5);
+		widths.put('$', 5);
+		widths.put('%', 5);
+		widths.put('&', 5);
+		widths.put('\'', 1);
+		widths.put('(', 4);
+		widths.put(')', 4);
+		widths.put('*', 3);
+		widths.put('+', 5);
+		widths.put(',', 1);
+		widths.put('-', 5);
+		widths.put('.', 1);
+		widths.put('/', 5);
+		
+		//48-57
+		widths.put('0', 5);
+		widths.put('1', 5);
+		widths.put('2', 5);
+		widths.put('3', 5);
+		widths.put('4', 5);
+		widths.put('5', 5);
+		widths.put('6', 5);
+		widths.put('7', 5);
+		widths.put('8', 5);
+		widths.put('9', 5);
+		
+		//58-64
+		widths.put(':', 1);
+		widths.put(';', 1);
+		widths.put('<', 4);
+		widths.put('=', 5);
+		widths.put('>', 4);
+		widths.put('?', 5);
+		widths.put('@', 6);
+		
+		//65-90
 		widths.put('A', 5);
 		widths.put('B', 5);
 		widths.put('C', 5);
@@ -43,90 +83,80 @@ public class AlignedSuffix implements SimpleFeature{
 		widths.put('X', 5);
 		widths.put('Y', 5);
 		widths.put('Z', 5);
+		
+		//91-96
+		widths.put('[', 3);
+		widths.put('\\', 5);
+		widths.put(']', 3);
+		widths.put('^', 5);
+		widths.put('_', 5);
+		widths.put('`', 2);
 
+		//97-122
 		widths.put('a', 5);
-		widths.put('á', 5);
-		widths.put('ä', 5);
 		widths.put('b', 5);
 		widths.put('c', 5);
-		widths.put('č', 3);
-		widths.put('ç', 5);
 		widths.put('d', 5);
-		widths.put('ď', 5);
 		widths.put('e', 5);
-		widths.put('é', 5);
-		widths.put('ě', 5);
 		widths.put('f', 4);
 		widths.put('g', 5);
 		widths.put('h', 5);
 		widths.put('i', 1);
-		widths.put('í', 1);
-		widths.put('ı', 2); //more like 2.5
 		widths.put('j', 5);
 		widths.put('k', 4);
 		widths.put('l', 2);
-		widths.put('ľ', 2);
-		widths.put('ĺ', 2);
 		widths.put('m', 5);
 		widths.put('n', 5);
-		widths.put('ň', 5);
 		widths.put('o', 5);
-		widths.put('ó', 5);
-		widths.put('ö', 5);
-		widths.put('ô', 5);
 		widths.put('p', 5);
 		widths.put('q', 5);
 		widths.put('r', 5);
-		widths.put('ŕ', 5);
-		widths.put('ř', 5);
 		widths.put('s', 5);
-		widths.put('š', 5);
-		widths.put('ş', 5);	
 		widths.put('t', 3);
-		widths.put('ť', 3);
 		widths.put('u', 5);
-		widths.put('ü', 5);
-		widths.put('ú', 5);
-		widths.put('ů', 5);
 		widths.put('v', 5);
 		widths.put('w', 5);
 		widths.put('x', 5);
 		widths.put('y', 5);
-		widths.put('ý', 5);
 		widths.put('z', 5);
+		
+		//123-126
+		widths.put('{', 4);
+		widths.put('|', 1);
+		widths.put('}', 4);
+		widths.put('~', 6);
+		
+		//extra
+		widths.put('á', 5);
+		widths.put('ä', 5);
+		widths.put('č', 3);
+		widths.put('ç', 5);
+		widths.put('ď', 5);
+		widths.put('é', 5);
+		widths.put('ě', 5);
+		widths.put('í', 1);
+		widths.put('ı', 2); //more like 2.5
+		widths.put('ľ', 2);
+		widths.put('ĺ', 2);
+		widths.put('ň', 5);
+		widths.put('ó', 5);
+		widths.put('ö', 5);
+		widths.put('ô', 5);
+		widths.put('ŕ', 5);
+		widths.put('ř', 5);
+		widths.put('š', 5);
+		widths.put('ş', 5);
+		widths.put('ť', 3);
+		widths.put('ü', 5);
+		widths.put('ú', 5);
+		widths.put('ů', 5);
+		widths.put('ý', 5);
 		widths.put('ž', 5);
-
-		widths.put('0', 5);
-		widths.put('1', 5);
-		widths.put('2', 5);
-		widths.put('3', 5);
-		widths.put('4', 5);
-		widths.put('5', 5);
-		widths.put('6', 5);
-		widths.put('7', 5);
-		widths.put('8', 5);
-		widths.put('9', 5);
-
-		widths.put('+', 5);
-		widths.put('*', 3);
-		widths.put('%', 5);
-		widths.put('(', 4);
-		widths.put(')', 4);
-		widths.put('[', 3);
-		widths.put(']', 3);
-		widths.put(',', 1);
-		widths.put('.', 1);
-		widths.put('-', 5);
-		widths.put('=', 5);
-		widths.put('_', 5);
-		widths.put(' ', 3);
 		widths.put((char)9876, 7); //crossed swords
 		widths.put((char)12304, 8);
 		widths.put((char)12305, 8);
 		widths.put((char)10084, 7);
 		widths.put((char)10004, 8);
-		widths.put('#', 5);
-		widths.put('!', 1);
 	}
 	public String fixTextWidth(ITabPlayer player, String prefixAndName, String suffix) {
 		int playerNameWidth = getTextWidth(IChatBaseComponent.fromColoredText(prefixAndName + suffix));
