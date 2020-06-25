5c8c5775a292f6acc26451285e766c5c397e22e9
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2020-06-25 18:14:20 +0200

    fixed NPE when command is used from console

--- src/me/neznamy/tab/shared/command/level1/ScoreboardCommand.java
+++ src/me/neznamy/tab/shared/command/level1/ScoreboardCommand.java
@@ -17,7 +17,7 @@ public class ScoreboardCommand extends SubCommand{
 	public void execute(ITabPlayer sender, String[] args) {
 		ScoreboardManager scoreboard = (ScoreboardManager) Shared.features.get("scoreboard");
 		if (scoreboard == null) {
-			sender.sendMessage(Placeholders.color("&cScoreboard feature is not enabled, therefore toggle command cannot be used."));
+			sendMessage(sender, Placeholders.color("&cScoreboard feature is not enabled, therefore toggle command cannot be used."));
 			return;
 		}
 		if (args.length == 0) {
