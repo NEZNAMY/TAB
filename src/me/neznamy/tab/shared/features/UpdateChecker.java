fe0ec86bb1d7704c747ab0e6c3c1df4ad7379383
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2020-06-30 16:22:03 +0200

    2.8.2

--- src/me/neznamy/tab/shared/features/UpdateChecker.java
+++ src/me/neznamy/tab/shared/features/UpdateChecker.java
@@ -10,7 +10,7 @@ import me.neznamy.tab.shared.Shared;
 
 public class UpdateChecker {
 
-	private final int currentVersionId = 281;
+	private final int currentVersionId = 282;
 	
 	public UpdateChecker() {
 		new Thread(new Runnable() {
