679cfbffa36cd75fc3b0c96ae3c129a9c756367d
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2020-07-11 21:03:02 +0200

    added config option to bypass "do-not-move-spectators" with a permission

--- src/me/neznamy/tab/shared/features/SpectatorFix.java
+++ src/me/neznamy/tab/shared/features/SpectatorFix.java
@@ -1,5 +1,6 @@
 package me.neznamy.tab.shared.features;
 
+import me.neznamy.tab.shared.Configs;
 import me.neznamy.tab.shared.ITabPlayer;
 import me.neznamy.tab.shared.cpu.CPUFeature;
 import me.neznamy.tab.shared.features.interfaces.PlayerInfoPacketListener;
@@ -8,11 +9,18 @@ import me.neznamy.tab.shared.packets.PacketPlayOutPlayerInfo.EnumGamemode;
 import me.neznamy.tab.shared.packets.PacketPlayOutPlayerInfo.EnumPlayerInfoAction;
 import me.neznamy.tab.shared.packets.PacketPlayOutPlayerInfo.PlayerInfoData;
 
-public class SpectatorFix implements PlayerInfoPacketListener{
+public class SpectatorFix implements PlayerInfoPacketListener {
 
+	private boolean allowBypass;
+	
+	public SpectatorFix() {
+		allowBypass = Configs.config.getBoolean("allow-spectator-bypass-permission", false);
+	}
+	
 	@Override
 	public PacketPlayOutPlayerInfo onPacketSend(ITabPlayer receiver, PacketPlayOutPlayerInfo info) {
 		if (receiver.getVersion().getMinorVersion() < 8) return info;
+		if (allowBypass && receiver.hasPermission("tab.spectatorbypass")) return info;
 		if (info.action != EnumPlayerInfoAction.UPDATE_GAME_MODE && info.action != EnumPlayerInfoAction.ADD_PLAYER) return info;
 		for (PlayerInfoData playerInfoData : info.entries) {
 			if (playerInfoData.gameMode == EnumGamemode.SPECTATOR && playerInfoData.uniqueId != receiver.getTablistId()) playerInfoData.gameMode = EnumGamemode.CREATIVE;
