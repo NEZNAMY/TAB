ddca3ad809735f6f5c03c0b6ac5e73b3d6f35c09
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2020-07-13 15:55:16 +0200

    using ConcurrentHashMap to avoid ConcurrentModificationException

--- src/me/neznamy/tab/platforms/bukkit/features/unlimitedtags/NameTagX.java
+++ src/me/neznamy/tab/platforms/bukkit/features/unlimitedtags/NameTagX.java
@@ -4,7 +4,6 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -66,7 +65,7 @@ public class NameTagX implements Listener, Loadable, JoinEventListener, QuitEven
 	private Field PacketPlayOutAttachEntity_PASSENGER;
 	private Field PacketPlayOutAttachEntity_VEHICLE;
 	
-	private Map<Integer, List<Integer>> vehicles = new HashMap<>();
+	private Map<Integer, List<Integer>> vehicles = new ConcurrentHashMap<>();
 
 	@SuppressWarnings("unchecked")
 	public NameTagX() {
