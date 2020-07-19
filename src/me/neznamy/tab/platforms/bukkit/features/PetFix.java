afe268f66c1a48c6969f2f5705c00dfc94ad50ca
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2020-07-19 23:00:14 +0200

    decent code cleanup

--- src/me/neznamy/tab/platforms/bukkit/features/PetFix.java
+++ src/me/neznamy/tab/platforms/bukkit/features/PetFix.java
@@ -3,7 +3,6 @@ package me.neznamy.tab.platforms.bukkit.features;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 import me.neznamy.tab.platforms.bukkit.packets.DataWatcher;
 import me.neznamy.tab.platforms.bukkit.packets.PacketPlayOut;
@@ -22,7 +21,7 @@ public class PetFix implements RawPacketFeature{
 	
 	private int getPetOwnerPosition() {
 		if (ProtocolVersion.SERVER_VERSION.getMinorVersion() >= 15) {
-			//1.15.x, 1.16.x
+			//1.15.x, 1.16.1
 			return 17;
 		} else if (ProtocolVersion.SERVER_VERSION.getMinorVersion() >= 14) {
 			//1.14.x
@@ -54,38 +53,16 @@ public class PetFix implements RawPacketFeature{
 			List<Object> newList = new ArrayList<Object>();
 			for (Object item : items) {
 				Item i = Item.fromNMS(item);
-				if (i.type.position == PET_OWNER_POSITION) {
-					modifyDataWatcherItem(i);
-				}
-				newList.add(i.toNMS());
+				if (i.type.position != PET_OWNER_POSITION) newList.add(i.toNMS());
 			}
 			PacketPlayOutEntityMetadata_LIST.set(packet, newList);
 		}
 		if (MethodAPI.PacketPlayOutSpawnEntityLiving.isInstance(packet) && PacketPlayOutSpawnEntityLiving.DATAWATCHER != null) {
 			DataWatcher watcher = DataWatcher.fromNMS(PacketPlayOutSpawnEntityLiving.DATAWATCHER.get(packet));
-			Item petOwner = watcher.getItem(PET_OWNER_POSITION);
-			if (petOwner != null) modifyDataWatcherItem(petOwner);
+			watcher.removeValue(PET_OWNER_POSITION);
 			PacketPlayOutSpawnEntityLiving.DATAWATCHER.set(packet, watcher.toNMS());
 		}
 		return packet;
-	}
-	
-	@SuppressWarnings({ "rawtypes" })
-	private void modifyDataWatcherItem(Item petOwner) {
-		//1.12-
-		if (petOwner.value instanceof com.google.common.base.Optional) {
-			com.google.common.base.Optional o = (com.google.common.base.Optional) petOwner.value;
-			if (o.isPresent() && o.get() instanceof UUID) {
-				petOwner.value = com.google.common.base.Optional.of(UUID.randomUUID());
-			}
-		}
-		//1.13+
-		if (petOwner.value instanceof java.util.Optional) {
-			java.util.Optional o = (java.util.Optional) petOwner.value;
-			if (o.isPresent() && o.get() instanceof UUID) {
-				petOwner.value = java.util.Optional.of(UUID.randomUUID());
-			}
-		}
 	}
 	
 	@Override
