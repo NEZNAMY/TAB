5f9a94796cb80804187ed4a016ac6aa7eb907286
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2020-08-06 17:04:46 +0200

    fuck denizen

--- src/me/neznamy/tab/platforms/bukkit/packets/NMSHook.java
+++ src/me/neznamy/tab/platforms/bukkit/packets/NMSHook.java
@@ -56,7 +56,7 @@ public class NMSHook {
 	public static Object getChannel(Player p){
 		try {
 			Object connection = PLAYER_CONNECTION.get(p.getClass().getMethod("getHandle").invoke(p));
-			Object networkManager = connection.getClass().getDeclaredField("networkManager").get(connection);
+			Object networkManager = connection.getClass().getField("networkManager").get(connection);
 			return CHANNEL.get(networkManager);
 		} catch (Exception e) {
 			return null;
