d827afe280f161773bad9270e1405e6513caea52
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2020-08-15 18:30:58 +0200

    showing the real exception

--- src/main/java/me/neznamy/tab/platforms/bukkit/permission/PermissionsEx.java
+++ src/main/java/me/neznamy/tab/platforms/bukkit/permission/PermissionsEx.java
@@ -1,20 +1,26 @@
 package me.neznamy.tab.platforms.bukkit.permission;
 
+import java.lang.reflect.InvocationTargetException;
+
 import me.neznamy.tab.shared.ITabPlayer;
 import me.neznamy.tab.shared.permission.PermissionPlugin;
 
 public class PermissionsEx implements PermissionPlugin {
 
 	@Override
-	public String getPrimaryGroup(ITabPlayer p) throws Exception {
+	public String getPrimaryGroup(ITabPlayer p) throws Throwable {
 		String[] groups = getAllGroups(p);
 		if (groups.length == 0) return "null";
 		return groups[0];
 	}
 
 	@Override
-	public String[] getAllGroups(ITabPlayer p) throws Exception {
-		Object user = Class.forName("ru.tehkode.permissions.bukkit.PermissionsEx").getMethod("getUser", String.class).invoke(null, p.getName());
-		return (String[]) user.getClass().getMethod("getGroupNames").invoke(user);
+	public String[] getAllGroups(ITabPlayer p) throws Throwable {
+		try {
+			Object user = Class.forName("ru.tehkode.permissions.bukkit.PermissionsEx").getMethod("getUser", String.class).invoke(null, p.getName());
+			return (String[]) user.getClass().getMethod("getGroupNames").invoke(user);
+		} catch (InvocationTargetException e) {
+			throw e.getCause();
+		}
 	}
 }
