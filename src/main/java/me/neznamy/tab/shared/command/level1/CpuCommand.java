81125c62d84cd024a6953b83cfe179c2b59d344e
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2020-08-31 00:22:35 +0200

    moved strings into constants for easier editing

--- src/main/java/me/neznamy/tab/shared/command/level1/CpuCommand.java
+++ src/main/java/me/neznamy/tab/shared/command/level1/CpuCommand.java
@@ -18,7 +18,22 @@ import me.neznamy.tab.shared.placeholders.RelationalPlaceholder;
  */
 public class CpuCommand extends SubCommand {
 
-	private DecimalFormat decimal3 = new DecimalFormat("#.###");
+	private final DecimalFormat decimal3 = new DecimalFormat("#.###");
+	
+	private final String SEPARATOR = "&8&l•&8&m                                                    ";
+	private final String HEADER_FOOTER = "&8&l•&8&m             &r&8&l[ &bTAB CPU Stats &8&l]&r&8&l&m             ";
+	private final String TITLE = "&8&l• &6CPU stats from the last minute";
+	private final String PLACEHOLDERS_TITLE = "&8&l• &6Placeholders:";
+	private final String PLACEHOLDER_LINE = "&8&l• &7%identifier% - %usage%%";
+	private final String BUKKIT_BRIDGE_TITLE = "&8&l• &6Placeholder usage on Bukkit servers:";
+	private final String BUKKIT_BRIDGE_TITLE_LINE = "&8&l• &7%identifier% - %usage%%";
+	private final String FEATURES_TITLE = "&8&l• &6Features:";
+	private final String FEATURE_NAME = "&8&l• &7%name%:";
+	private final String FEATURE_LINE = "&8&l•     &7%category% - %usage%%";
+	private final String PLACEHOLDERS_TOTAL = "&8&l• &6&lPlaceholders Total: &a&l%total%%";
+	private final String BRIDGE_PLACEHOLDERS_TOTAL = "&8&l• &6&lBukkit bridge placeholders Total: &a&l%total%%";
+	private final String PLUGIN_INTERNALS = "&8&l• &6&lPlugin internals: &a&l%total%%";
+	private final String TOTAL = "&8&l• &6&lTotal: &e&l%total%%";
 	
 	public CpuCommand() {
 		super("cpu", "tab.cpu");
@@ -43,10 +58,10 @@ public class CpuCommand extends SubCommand {
 		}
 		
 		sendMessage(sender, " ");
-		sendMessage(sender, "&8&l•&8&m             &r&8&l[ &bTAB CPU Stats &8&l]&r&8&l&m             ");
-		sendMessage(sender, "&8&l• &6CPU stats from the last minute");
-		sendMessage(sender, "&8&l•&8&m                                                    ");
-		sendMessage(sender, "&8&l• &6Placeholders:");
+		sendMessage(sender, HEADER_FOOTER);
+		sendMessage(sender, TITLE);
+		sendMessage(sender, SEPARATOR);
+		sendMessage(sender, PLACEHOLDERS_TITLE);
 		for (Entry<String, Float> entry : placeholders.entrySet()) {
 			String refresh = "";
 			if (!entry.getKey().toString().startsWith("%rel_")) {
@@ -56,29 +71,31 @@ public class CpuCommand extends SubCommand {
 				RelationalPlaceholder rel = Placeholders.getRelationalPlaceholder(entry.getKey()+"");
 				if (rel != null) refresh = " &8(" + rel.refresh + ")&7";
 			}
-			sendMessage(sender, "&8&l• &7" + entry.getKey() + refresh + " - " + colorizePlaceholder(decimal3.format(entry.getValue())) + "%");
+			sendMessage(sender, PLACEHOLDER_LINE.replace("%identifier%", entry.getKey() + refresh).replace("%usage%", colorizePlaceholder(decimal3.format(entry.getValue()))));
 		}
-		sendMessage(sender, "&8&l•&8&m                                                    ");
+		sendMessage(sender, SEPARATOR);
 		if (Shared.platform.getSeparatorType().equals("server")) {
-			sendMessage(sender, "&8&l• &6Placeholder usage on Bukkit servers:");
+			sendMessage(sender, BUKKIT_BRIDGE_TITLE);
 			for (Entry<String, Float> entry : bridgeplaceholders.entrySet()) {
-				sendMessage(sender, "&8&l• &7" + entry.getKey() + " - " + colorizePlaceholder(decimal3.format(entry.getValue())) + "%");
+				sendMessage(sender, BUKKIT_BRIDGE_TITLE_LINE.replace("%identifier%", entry.getKey()).replace("%usage%", colorizePlaceholder(decimal3.format(entry.getValue()))));
 			}
-			sendMessage(sender, "&8&l•&8&m                                                    ");
+			sendMessage(sender, SEPARATOR);
 		}
-		sendMessage(sender, "&8&l• &6Features:");
+		sendMessage(sender, FEATURES_TITLE);
 		for (Entry<TabFeature, Map<UsageType, Float>> entry : features.entrySet()) {
-			sendMessage(sender, "&8&l• &7" + entry.getKey() + ":");
+			sendMessage(sender, FEATURE_NAME.replace("%name%", entry.getKey().toString()));
 			for (Entry<UsageType, Float> type : entry.getValue().entrySet()){
-				sendMessage(sender, "&8&l•     &7" + type.getKey() + " - " + colorizeFeature(decimal3.format(type.getValue())) + "%");
+				sendMessage(sender, FEATURE_LINE.replace("%category%", type.getKey().toString()).replace("%usage%", colorizeFeature(decimal3.format(type.getValue()))));
 			}
 		}
-		sendMessage(sender, "&8&l•&8&m                                                    ");
-		sendMessage(sender, "&8&l• &6&lPlaceholders Total: &a&l" + colorizeTotalUsage(decimal3.format(placeholdersTotal)) + "%");
-		if (Shared.platform.getSeparatorType().equals("server")) sendMessage(sender, "&8&l• &6&lBukkit bridge placeholders Total: &a&l" + colorizeTotalUsage(decimal3.format(bridgeplaceholdersTotal)) + "%");
-		sendMessage(sender, "&8&l• &6&lPlugin internals: &a&l" + colorizeTotalUsage(decimal3.format(featuresTotal-placeholdersTotal)) + "%");
-		sendMessage(sender, "&8&l• &6&lTotal: &e&l" + colorizeTotalUsage(decimal3.format(featuresTotal + bridgeplaceholdersTotal)) + "%");
-		sendMessage(sender, "&8&l•&8&m             &r&8&l[ &bTAB CPU Stats &8&l]&r&8&l&m             ");
+		sendMessage(sender, SEPARATOR);
+		sendMessage(sender, PLACEHOLDERS_TOTAL.replace("%total%", colorizeTotalUsage(decimal3.format(placeholdersTotal))));
+		if (Shared.platform.getSeparatorType().equals("server")) {
+			sendMessage(sender, BRIDGE_PLACEHOLDERS_TOTAL.replace("%total%", colorizeTotalUsage(decimal3.format(bridgeplaceholdersTotal)) + "%"));
+		}
+		sendMessage(sender, PLUGIN_INTERNALS.replace("%total%", colorizeTotalUsage(decimal3.format(featuresTotal-placeholdersTotal)) + "%"));
+		sendMessage(sender, TOTAL.replace("%total%", colorizeTotalUsage(decimal3.format(featuresTotal + bridgeplaceholdersTotal)) + "%"));
+		sendMessage(sender, HEADER_FOOTER);
 		sendMessage(sender, " ");
 	}
 	private String colorizePlaceholder(String usage) {
