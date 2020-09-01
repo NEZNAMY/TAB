1d00c0827a07de323550d40f20ca9ba1b1632ef5
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2020-09-01 22:54:03 +0200

    2.8.5-pre7

--- src/main/java/me/neznamy/tab/shared/Shared.java
+++ src/main/java/me/neznamy/tab/shared/Shared.java
@@ -26,7 +26,7 @@ public class Shared {
 
 	public static final String DECODER_NAME = "TABReader";
 	public static final String CHANNEL_NAME = "tab:placeholders";
-	public static final String pluginVersion = "2.8.5-pre6";
+	public static final String pluginVersion = "2.8.5-pre7";
 
 	public static final Map<UUID, ITabPlayer> data = new ConcurrentHashMap<UUID, ITabPlayer>();
 	public static final Map<Integer, ITabPlayer> entityIdMap = new ConcurrentHashMap<Integer, ITabPlayer>();
