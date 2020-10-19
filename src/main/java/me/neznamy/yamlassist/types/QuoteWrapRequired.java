6be627933e0dda28ec59ca64b50a95dd007bf555
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2020-10-19 16:44:10 +0200

    fixed false positive due to misleading error message from yaml parser

--- src/main/java/me/neznamy/yamlassist/types/QuoteWrapRequired.java
+++ src/main/java/me/neznamy/yamlassist/types/QuoteWrapRequired.java
@@ -18,8 +18,22 @@ public class QuoteWrapRequired extends SyntaxError {
 		if (exception.getMessage().contains("expected alphabetic or numeric character") ||
 			exception.getMessage().contains("Do not use %")) {
 			int line = Integer.parseInt(exception.getMessage().split(", line ")[1].split(",")[0]);
-			suggestions.add("Wrap value in line " + line + " into quotes.");
+			String value = getValue(fileLines.get(line-1));
+			//avoiding false positive caused by missing ending quote in previous line
+			if (!value.startsWith("\"") && !value.startsWith("'")) {
+				suggestions.add("Wrap value in line " + line + " into quotes.");
+			}
 		}
 		return suggestions;
+	}
+	
+	private String getValue(String line) {
+		if (line.startsWith("- ")) {
+			return line.substring(line.split("- ")[0].length()+2);
+		} else if (line.contains(": ")) {
+			return line.substring(line.split(": ")[0].length()+2);
+		}
+		//should not happen
+		return line;
 	}
 }
