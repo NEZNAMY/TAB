db3d154acf13896d56275f863c4aacf50db3f3d2
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2020-11-15 13:11:43 +0100

    fixed error

--- src/main/java/me/neznamy/tab/premium/scoreboard/lines/StableDynamicLine.java
+++ src/main/java/me/neznamy/tab/premium/scoreboard/lines/StableDynamicLine.java
@@ -69,8 +69,9 @@ public abstract class StableDynamicLine extends ScoreboardLine {
 				
 		if (replaced.length() > charLimit && p.getVersion().getMinorVersion() < 13) {
 			replaced = IChatBaseComponent.fromColoredText(replaced).toLegacyText(); //converting RGB to legacy here to avoid splitting in the middle of RGB code
-			prefix = replaced.substring(0, charLimit);
-			suffix = replaced.substring(charLimit, replaced.length());
+			int prefixLength = Math.min(charLimit, replaced.length()); //in case of short RGB text and it changed length
+			prefix = replaced.substring(0, prefixLength);
+			suffix = replaced.substring(prefixLength, replaced.length());
 			if (prefix.charAt(charLimit-1) == Placeholders.colorChar) {
 				prefix = prefix.substring(0, charLimit-1);
 				suffix = Placeholders.colorChar + suffix;
