1f206cdfab7af8dd9087271f01a50beb2db499f0
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2020-11-23 09:13:42 +0100

    help menu now correctly shows /btab on bungee

--- src/main/java/me/neznamy/tab/shared/command/DisabledCommand.java
+++ src/main/java/me/neznamy/tab/shared/command/DisabledCommand.java
@@ -24,9 +24,10 @@ public class DisabledCommand {
 			}
 		} else {
 			if (hasAdminPermission) {
+				String command = Shared.platform.getSeparatorType().equals("world") ? "/tab" : "/btab";
 				messages.add("&m                                                                                ");
 				messages.add(" &cPlugin is disabled due to a broken configuration file (" + Shared.brokenFile + "). Check console for more details.");
-				messages.add(" &8>> &3&l/tab reload");
+				messages.add(" &8>> &3&l" + command + " reload");
 				messages.add("      - &7Reloads plugin and config");
 				messages.add("&m                                                                                ");
 			}
