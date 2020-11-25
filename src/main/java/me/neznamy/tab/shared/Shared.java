52a4a3453fb47fbda170f9763d48d9e86dc9f046
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2020-11-25 23:38:05 +0100

    2.8.10-pre1

--- src/main/java/me/neznamy/tab/shared/Shared.java
+++ src/main/java/me/neznamy/tab/shared/Shared.java
@@ -29,7 +29,7 @@ public class Shared {
 	public static final String CHANNEL_NAME = "tab:placeholders";
 	
 	//version of plugin
-	public static final String pluginVersion = "2.8.9";
+	public static final String pluginVersion = "2.8.10-pre1";
 
 	//player data
 	public static final Map<UUID, TabPlayer> data = new ConcurrentHashMap<UUID, TabPlayer>();
