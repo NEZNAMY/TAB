8a218886156b0946a5586a20f02a4e9ad8a08d17
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2020-12-05 22:50:11 +0100

    added try/catch

--- src/main/java/me/neznamy/tab/platforms/bungee/BungeePipelineInjector.java
+++ src/main/java/me/neznamy/tab/platforms/bungee/BungeePipelineInjector.java
@@ -1,6 +1,7 @@
 package me.neznamy.tab.platforms.bungee;
 
 import java.util.Collection;
+import java.util.NoSuchElementException;
 import java.util.UUID;
 
 import com.google.common.collect.Lists;
@@ -20,80 +21,84 @@ import net.md_5.bungee.protocol.packet.PlayerListItem;
 import net.md_5.bungee.protocol.packet.Team;
 
 public class BungeePipelineInjector extends PipelineInjector {
-	
+
 	private static final String INJECT_POSITION = "inbound-boss";
-	
+
 	@Override
 	public void inject(UUID uuid) {
 		TabPlayer p = Shared.getPlayer(uuid);
 		if (p.getVersion().getMinorVersion() < 8) return;
 		if (p.getChannel().pipeline().names().contains(DECODER_NAME)) p.getChannel().pipeline().remove(DECODER_NAME);
-		p.getChannel().pipeline().addBefore(INJECT_POSITION, DECODER_NAME, new ChannelDuplexHandler() {
+		try {
+			p.getChannel().pipeline().addBefore(INJECT_POSITION, DECODER_NAME, new ChannelDuplexHandler() {
 
-			public void write(ChannelHandlerContext context, Object packet, ChannelPromise channelPromise) throws Exception {
-				TabPlayer player = Shared.getPlayer(uuid);
-				if (player == null) {
-					super.write(context, packet, channelPromise);
-					return;
-				}
-				try {
-					if (packet instanceof PlayerListItem) {
-						super.write(context, Shared.featureManager.onPacketPlayOutPlayerInfo(player, packet), channelPromise);
+				public void write(ChannelHandlerContext context, Object packet, ChannelPromise channelPromise) throws Exception {
+					TabPlayer player = Shared.getPlayer(uuid);
+					if (player == null) {
+						super.write(context, packet, channelPromise);
 						return;
 					}
-					if (Shared.featureManager.isFeatureEnabled("nametag16")) {
-						long time = System.nanoTime();
-						if (packet instanceof Team) {
-							//team packet coming from a bungeecord plugin
-							modifyPlayers((Team) packet);
-							Shared.cpu.addTime(TabFeature.NAMETAGS, UsageType.ANTI_OVERRIDE, System.nanoTime()-time);
-							super.write(context, packet, channelPromise);
+					try {
+						if (packet instanceof PlayerListItem) {
+							super.write(context, Shared.featureManager.onPacketPlayOutPlayerInfo(player, packet), channelPromise);
 							return;
 						}
-						if (packet instanceof ByteBuf) {
-							ByteBuf buf = ((ByteBuf) packet);
-							int marker = buf.readerIndex();
-							int packetId = buf.readByte();
-							if (packetId == ((BungeeTabPlayer)player).getPacketId(Team.class)) {
-								//team packet sent by backend server, proxy does not deserialize those for whatever reason
-								Team team = new Team();
-								team.read(buf, null, ((ProxiedPlayer)player.getPlayer()).getPendingConnection().getVersion());
-								buf.release();
-								modifyPlayers(team);
+						if (Shared.featureManager.isFeatureEnabled("nametag16")) {
+							long time = System.nanoTime();
+							if (packet instanceof Team) {
+								//team packet coming from a bungeecord plugin
+								modifyPlayers((Team) packet);
 								Shared.cpu.addTime(TabFeature.NAMETAGS, UsageType.ANTI_OVERRIDE, System.nanoTime()-time);
-								super.write(context, team, channelPromise);
-								return;
-							} else if (packetId + 128 == ((BungeeTabPlayer)player).getPacketId(Team.class)){
-								//compressed team packet when using protocolsupport, just kill it as it does not come from tab anyway
-								buf.release();
+								super.write(context, packet, channelPromise);
 								return;
 							}
-							buf.readerIndex(marker);
+							if (packet instanceof ByteBuf) {
+								ByteBuf buf = ((ByteBuf) packet);
+								int marker = buf.readerIndex();
+								int packetId = buf.readByte();
+								if (packetId == ((BungeeTabPlayer)player).getPacketId(Team.class)) {
+									//team packet sent by backend server, proxy does not deserialize those for whatever reason
+									Team team = new Team();
+									team.read(buf, null, ((ProxiedPlayer)player.getPlayer()).getPendingConnection().getVersion());
+									buf.release();
+									modifyPlayers(team);
+									Shared.cpu.addTime(TabFeature.NAMETAGS, UsageType.ANTI_OVERRIDE, System.nanoTime()-time);
+									super.write(context, team, channelPromise);
+									return;
+								} else if (packetId + 128 == ((BungeeTabPlayer)player).getPacketId(Team.class)){
+									//compressed team packet when using protocolsupport, just kill it as it does not come from tab anyway
+									buf.release();
+									return;
+								}
+								buf.readerIndex(marker);
+							}
+							Shared.cpu.addTime(TabFeature.NAMETAGS, UsageType.ANTI_OVERRIDE, System.nanoTime()-time);
 						}
-						Shared.cpu.addTime(TabFeature.NAMETAGS, UsageType.ANTI_OVERRIDE, System.nanoTime()-time);
+						//client reset packet
+						if (packet instanceof Login) {
+							//making sure to not send own packets before login packet is actually sent
+							super.write(context, packet, channelPromise);
+							Shared.featureManager.onLoginPacket(player);
+							return;
+						}
+					} catch (Throwable e){
+						Shared.errorManager.printError("An error occurred when analyzing packets for player " + player.getName() + " with client version " + player.getVersion().getFriendlyName(), e);
 					}
-					//client reset packet
-					if (packet instanceof Login) {
-						//making sure to not send own packets before login packet is actually sent
-						super.write(context, packet, channelPromise);
-						Shared.featureManager.onLoginPacket(player);
-						return;
-					}
-				} catch (Throwable e){
-					Shared.errorManager.printError("An error occurred when analyzing packets for player " + player.getName() + " with client version " + player.getVersion().getFriendlyName(), e);
+					super.write(context, packet, channelPromise);
 				}
-				super.write(context, packet, channelPromise);
-			}
-		});
+			});
+		} catch (NoSuchElementException | IllegalArgumentException e) {
+			//idk how does this keep happening but whatever
+		}
 	}
-	
+
 	@Override
 	public void uninject(UUID uuid) {
 		TabPlayer p = Shared.getPlayer(uuid);
 		if (p.getVersion().getMinorVersion() < 8) return;
 		if (p.getChannel().pipeline().names().contains(DECODER_NAME)) p.getChannel().pipeline().remove(DECODER_NAME);
 	}
-	
+
 	/**
 	 * Removes all real players from packet if the packet doesn't come from TAB
 	 * @param packet - packet to modify
