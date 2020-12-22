d5861332b9482dbdbe6a34f763b2f238461d6481
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2020-12-22 10:35:08 +0100

    no longer sending empty packets due to NPCs being added into tablist

--- src/main/java/me/neznamy/tab/shared/features/Playerlist.java
+++ src/main/java/me/neznamy/tab/shared/features/Playerlist.java
@@ -77,7 +77,7 @@ public class Playerlist implements JoinEventListener, Loadable, WorldChangeListe
 			}
 			if (ADD && packetPlayer != null && receiver.getVersion() == ProtocolVersion.v1_8) v180PrefixBugFixList.add(playerInfoData.clone());
 		}
-		if (ADD && receiver.getVersion() == ProtocolVersion.v1_8) {
+		if (ADD && receiver.getVersion() == ProtocolVersion.v1_8 && !v180PrefixBugFixList.isEmpty()) { //not sending empty packets due to NPCs
 			//1.8.0 bug, sending to all 1.8.x clients as there is no way to find out if they use 1.8.0
 			Shared.cpu.runTaskLater(50, "sending PacketPlayOutPlayerInfo", getFeatureType(), UsageType.v1_8_0_BUG_COMPENSATION, () -> receiver.sendCustomPacket(new PacketPlayOutPlayerInfo(EnumPlayerInfoAction.UPDATE_DISPLAY_NAME, v180PrefixBugFixList)));
 		}
