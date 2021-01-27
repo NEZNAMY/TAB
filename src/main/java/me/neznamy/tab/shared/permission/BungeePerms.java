585ff999a2df8b7dfd48c36424abc173eb1c5e4d
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2021-01-27 13:37:41 +0100

    using reflection for BungeePerms
    the repository or at least the current version is gone, this will prevent the issue in the future as well

--- src/main/java/me/neznamy/tab/shared/permission/BungeePerms.java
+++ src/main/java/me/neznamy/tab/shared/permission/BungeePerms.java
@@ -2,10 +2,9 @@ package me.neznamy.tab.shared.permission;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 
 import me.neznamy.tab.api.TabPlayer;
-import net.alpenblock.bungeeperms.Group;
-import net.alpenblock.bungeeperms.PermissionsManager;
 
 /**
  * BungeePerms hook
@@ -18,20 +17,27 @@ public class BungeePerms implements PermissionPlugin {
 		this.version = version;
 	}
 	@Override
-	public String getPrimaryGroup(TabPlayer p) {
-		PermissionsManager pm = net.alpenblock.bungeeperms.BungeePerms.getInstance().getPermissionsManager();
-		return pm.getMainGroup(pm.getUser(p.getUniqueId())).getName();
+	public String getPrimaryGroup(TabPlayer p) throws Exception {
+		Object bungeePerms = Class.forName("net.alpenblock.bungeeperms.BungeePerms").getMethod("getInstance").invoke(null);
+		Object permissionsManager = bungeePerms.getClass().getMethod("getPermissionsManager").invoke(bungeePerms);
+		Object user = permissionsManager.getClass().getMethod("getUser", UUID.class).invoke(permissionsManager, p.getUniqueId());
+		Object group = permissionsManager.getClass().getMethod("getMainGroup", user.getClass()).invoke(permissionsManager, user);
+		return (String) group.getClass().getMethod("getName").invoke(group);
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
-	public String[] getAllGroups(TabPlayer p) {
-		PermissionsManager pm = net.alpenblock.bungeeperms.BungeePerms.getInstance().getPermissionsManager();
+	public String[] getAllGroups(TabPlayer p) throws Exception {
+		Object bungeePerms = Class.forName("net.alpenblock.bungeeperms.BungeePerms").getMethod("getInstance").invoke(null);
+		Object permissionsManager = bungeePerms.getClass().getMethod("getPermissionsManager").invoke(bungeePerms);
+		Object user = permissionsManager.getClass().getMethod("getUser", UUID.class).invoke(permissionsManager, p.getUniqueId());
 		Set<String> groups = new HashSet<String>();
-		for (Group group : pm.getUser(p.getUniqueId()).getGroups()) {
-			groups.add(group.getName());
+		for (Object group : (Iterable<Object>)user.getClass().getMethod("getGroups").invoke(user)) {
+			groups.add((String) group.getClass().getMethod("getName").invoke(group));
 		}
 		return groups.toArray(new String[0]);
 	}
+	
 	@Override
 	public String getVersion() {
 		return version;
