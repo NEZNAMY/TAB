cf0b0ed8d26b7fab8a0c1bff6dd6be7ce2260a43
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2021-03-16 15:47:31 +0100

    added missing legacy code pattern from PR

--- src/main/java/me/neznamy/tab/shared/rgb/gradient/KyoriGradient.java
+++ src/main/java/me/neznamy/tab/shared/rgb/gradient/KyoriGradient.java
@@ -3,6 +3,7 @@ package me.neznamy.tab.shared.rgb.gradient;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import me.neznamy.tab.shared.packets.EnumChatFormat;
 import me.neznamy.tab.shared.rgb.TextColor;
 
 /**
@@ -13,15 +14,29 @@ public class KyoriGradient extends GradientPattern {
 	// <gradient:#RRGGBB:#RRGGBB>Text</gradient>
 	private final Pattern pattern = Pattern.compile("<gradient:#[0-9a-fA-F]{6}:#[0-9a-fA-F]{6}>[^<]*</gradient>");
 	
+	// <gradient:#RRGGBB|L:#RRGGBB>Text</gradient>
+	private final Pattern legacy = Pattern.compile("<gradient:#[0-9a-fA-F]{6}\\|.:#[0-9a-fA-F]{6}>[^<]*</gradient>");
+	
 	@Override
 	public String applyPattern(String text) {
 		String replaced = text;
-		Matcher m = pattern.matcher(replaced);
+		Matcher m = legacy.matcher(replaced);
+		while (m.find()) {
+			String format = m.group();
+			EnumChatFormat legacyColor = EnumChatFormat.getByChar(format.charAt(18));
+			if (legacyColor == null) continue;
+			TextColor start = new TextColor(format.substring(11, 17), legacyColor);
+			String message = format.substring(28, format.length()-11);
+			TextColor end = new TextColor(format.substring(21, 27));
+			String applied = asGradient(start, message, end);
+			replaced = replaced.replace(format, applied);
+		}
+		m = pattern.matcher(replaced);
 		while (m.find()) {
 			String format = m.group();
 			TextColor start = new TextColor(format.substring(11, 17));
 			String message = format.substring(26, format.length()-11);
-			TextColor end = new TextColor(format.substring(19,25));
+			TextColor end = new TextColor(format.substring(19, 25));
 			String applied = asGradient(start, message, end);
 			replaced = replaced.replace(format, applied);
 		}
