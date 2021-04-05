cba4dbdf845b9b0c741a5f4cf0228fe02408d87f
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2021-04-05 20:40:55 +0200

    2.8.11-pre11

--- src/main/java/me/neznamy/tab/shared/TAB.java
+++ src/main/java/me/neznamy/tab/shared/TAB.java
@@ -25,7 +25,7 @@ public class TAB {
 	private static TAB instance;
 	
 	//version of plugin
-	public static final String pluginVersion = "2.8.11-pre10";
+	public static final String pluginVersion = "2.8.11-pre11";
 
 	//player data
 	public final Map<UUID, TabPlayer> data = new ConcurrentHashMap<UUID, TabPlayer>();
