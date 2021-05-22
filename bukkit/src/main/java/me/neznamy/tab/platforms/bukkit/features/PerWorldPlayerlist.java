8493b37171e6e8b9a1154abf3e748b2a9cb2fa1c
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2021-05-22 16:41:56 +0200

    recoded feature to fix issues, worked

--- bukkit/src/main/java/me/neznamy/tab/platforms/bukkit/features/PerWorldPlayerlist.java
+++ bukkit/src/main/java/me/neznamy/tab/platforms/bukkit/features/PerWorldPlayerlist.java
@@ -1,45 +1,35 @@
 package me.neznamy.tab.platforms.bukkit.features;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import me.neznamy.tab.api.TabPlayer;
 import me.neznamy.tab.shared.TAB;
 import me.neznamy.tab.shared.cpu.TabFeature;
+import me.neznamy.tab.shared.cpu.UsageType;
 import me.neznamy.tab.shared.features.types.Loadable;
-import me.neznamy.tab.shared.features.types.event.JoinEventListener;
-import me.neznamy.tab.shared.features.types.event.WorldChangeListener;
-import me.neznamy.tab.shared.features.types.packet.PlayerInfoPacketListener;
-import me.neznamy.tab.shared.packets.PacketPlayOutPlayerInfo;
-import me.neznamy.tab.shared.packets.PacketPlayOutPlayerInfo.EnumPlayerInfoAction;
-import me.neznamy.tab.shared.packets.PacketPlayOutPlayerInfo.PlayerInfoData;
 
 /**
- * Per-world-playerlist feature. Currently bukkit API based, however that causes various (compatibility) issues.
- * Will be reworked to use packets in the future
+ * Per-world-playerlist feature handler
  */
 @SuppressWarnings("deprecation")
-public class PerWorldPlayerlist implements Loadable, JoinEventListener, WorldChangeListener, PlayerInfoPacketListener {
-
-	//tab instance
-	private TAB tab;
+public class PerWorldPlayerlist implements Loadable, Listener {
 	
 	//plugin instance
 	private JavaPlugin plugin;
 	
-	//if bypass permission is allowed or not
+	//config options
 	private boolean allowBypass;
-	
-	//list of ignored worlds
 	private List<String> ignoredWorlds;
-	
-	//map of worlds that share playerlist
 	private Map<String, List<String>> sharedWorlds;
 
 	/**
@@ -49,7 +39,6 @@ public class PerWorldPlayerlist implements Loadable, JoinEventListener, WorldCha
 	 */
 	public PerWorldPlayerlist(JavaPlugin plugin, TAB tab) {
 		this.plugin = plugin;
-		this.tab = tab;
 		allowBypass = tab.getConfiguration().config.getBoolean("per-world-playerlist.allow-bypass-permission", false);
 		ignoredWorlds = tab.getConfiguration().config.getStringList("per-world-playerlist.ignore-effect-in-worlds", Arrays.asList("ignoredworld", "build"));
 		sharedWorlds = tab.getConfiguration().config.getConfigurationSection("per-world-playerlist.shared-playerlist-world-groups");
@@ -60,116 +49,74 @@ public class PerWorldPlayerlist implements Loadable, JoinEventListener, WorldCha
 			}
 		}
 		tab.debug(String.format("Loaded PerWorldPlayerlist feature with parameters allowBypass=%s, ignoredWorlds=%s, sharedWorlds=%s", allowBypass, ignoredWorlds, sharedWorlds));
+		Bukkit.getPluginManager().registerEvents(this, plugin);
 	}
 	
 	@Override
 	public void load(){
-		for (TabPlayer p : tab.getPlayers()){
-			hidePlayer(p);
-			showInSameWorldGroup(p);
-		}
+		Bukkit.getScheduler().runTask(plugin, new Runnable() {
+
+			@Override
+			public void run() {
+				Bukkit.getOnlinePlayers().forEach(p -> checkPlayer(p));
+			}
+		});
 	}
 	
 	@Override
 	public void unload(){
-		for (TabPlayer p : tab.getPlayers()) {
-			for (TabPlayer pl : tab.getPlayers()) {
-				((Player) p.getPlayer()).showPlayer((Player) pl.getPlayer());
+		for (Player p : Bukkit.getOnlinePlayers()) {
+			for (Player pl : Bukkit.getOnlinePlayers()) {
+				p.showPlayer(pl);
+			}
+		}
+		HandlerList.unregisterAll(this);
+	}
+	
+	@EventHandler
+	public void onJoin(PlayerJoinEvent e) {
+		long time = System.nanoTime();
+		checkPlayer(e.getPlayer());
+		TAB.getInstance().getCPUManager().addTime(TabFeature.PER_WORLD_PLAYERLIST, UsageType.PLAYER_JOIN_EVENT, System.nanoTime()-time);
+	}
+	
+	@EventHandler
+	public void onWorldChange(PlayerChangedWorldEvent e) {
+		long time = System.nanoTime();
+		checkPlayer(e.getPlayer());
+		TAB.getInstance().getCPUManager().addTime(TabFeature.PER_WORLD_PLAYERLIST, UsageType.WORLD_SWITCH_EVENT, System.nanoTime()-time);
+	}
+	
+	private void checkPlayer(Player p) {
+		for (Player all : Bukkit.getOnlinePlayers()){
+			if (all == p) continue;
+			if (!shouldSee(p, all) && p.canSee(all)) {
+				p.hidePlayer(all);
+			}
+			if (shouldSee(p, all) && !p.canSee(all)) {
+				p.showPlayer(all);
+			}
+			if (!shouldSee(all, p) && all.canSee(p)) {
+				all.hidePlayer(p);
+			}
+			if (shouldSee(all, p) && !all.canSee(p)) {
+				all.showPlayer(p);
 			}
 		}
 	}
 	
-	@Override
-	public void onJoin(TabPlayer connectedPlayer) {
-		hidePlayer(connectedPlayer);
-		showInSameWorldGroup(connectedPlayer);
-	}
-	
-	@Override
-	public void onWorldChange(TabPlayer p, String from, String to) {
-		hidePlayer(p);
-		showInSameWorldGroup(p);
-	}
-	
-	/**
-	 * Shows all players that player should see
-	 * @param shown - player to process mutual visibility for
-	 */
-	private void showInSameWorldGroup(TabPlayer shown){
-		Bukkit.getScheduler().runTask(plugin, new Runnable() {
-
-			@Override
-			public void run() {
-				try {
-					for (TabPlayer everyone : tab.getPlayers()){
-						if (everyone == shown) continue;
-						if (shouldSee(shown, everyone)) ((Player) shown.getPlayer()).showPlayer((Player) everyone.getPlayer());
-						if (shouldSee(everyone, shown)) ((Player) everyone.getPlayer()).showPlayer((Player) shown.getPlayer());
-					}
-				} catch (Throwable t) {
-					tab.getErrorManager().printError("Failed to show players", t);
-				}
-			}
-		});
-	}
-	
-	/**
-	 * Returns true if specified viewer should see player, false if not
-	 * @param viewer - tablist viewer
-	 * @param displayed - player to check if should be visible
-	 * @return true if should, false if not
-	 */
-	private boolean shouldSee(TabPlayer viewer, TabPlayer displayed) {
-		if (displayed == viewer) return true;
-		if ((allowBypass && viewer.hasPermission("tab.bypass")) || ignoredWorlds.contains(((Player)viewer.getPlayer()).getWorld().getName())) return true;
-		String viewerWorldGroup = ((Player)viewer.getPlayer()).getWorld().getName() + "-default"; //preventing unwanted behavior when some group is called exactly like a world
-		String targetWorldGroup = ((Player)displayed.getPlayer()).getWorld().getName() + "-default";
+	private boolean shouldSee(Player viewer, Player target) {
+		if (target == viewer) return true;
+		if ((allowBypass && viewer.hasPermission("tab.bypass")) || ignoredWorlds.contains(viewer.getWorld().getName())) return true;
+		String viewerWorldGroup = viewer.getWorld().getName() + "-default"; //preventing unwanted behavior when some group is called exactly like a world
+		String targetWorldGroup = target.getWorld().getName() + "-default";
 		for (String group : sharedWorlds.keySet()) {
 			if (sharedWorlds.get(group) != null) {
-				if (sharedWorlds.get(group).contains(((Player)viewer.getPlayer()).getWorld().getName())) viewerWorldGroup = group;
-				if (sharedWorlds.get(group).contains(((Player)displayed.getPlayer()).getWorld().getName())) targetWorldGroup = group;
+				if (sharedWorlds.get(group).contains(viewer.getWorld().getName())) viewerWorldGroup = group;
+				if (sharedWorlds.get(group).contains(target.getWorld().getName())) targetWorldGroup = group;
 			}
 		}
 		return viewerWorldGroup.equals(targetWorldGroup);
-	}
-	
-	/**
-	 * Hides the player from everyone and everyone to the player
-	 * @param hidden - player to hide
-	 */
-	private void hidePlayer(TabPlayer hidden){
-		Bukkit.getScheduler().runTask(plugin, new Runnable() {
-
-			@Override
-			public void run() {
-				try {
-					for (TabPlayer everyone : tab.getPlayers()){
-						if (everyone == hidden) continue;
-						((Player) hidden.getPlayer()).hidePlayer((Player) everyone.getPlayer());
-						((Player) everyone.getPlayer()).hidePlayer((Player) hidden.getPlayer());
-					}
-				} catch (Throwable t) {
-					tab.getErrorManager().printError("Failed to hide players", t);
-				}
-			}
-		});
-	}
-	
-	//fixing bukkit api bug making players not hide when hidePlayer is called too early
-	@Override
-	public void onPacketSend(TabPlayer receiver, PacketPlayOutPlayerInfo info) {
-		if (info.action != EnumPlayerInfoAction.ADD_PLAYER) return;
-		List<PlayerInfoData> toRemove = new ArrayList<PlayerInfoData>();
-		for (PlayerInfoData data : info.entries) {
-			TabPlayer added = tab.getPlayerByTablistUUID(data.uniqueId);
-			if (added != null && !shouldSee(receiver, added)) {
-				toRemove.add(data);
-			}
-		}
-		List<PlayerInfoData> newList = new ArrayList<PlayerInfoData>();
-		info.entries.forEach(d -> newList.add(d));
-		newList.removeAll(toRemove);
-		info.entries = newList;
 	}
 
 	@Override
