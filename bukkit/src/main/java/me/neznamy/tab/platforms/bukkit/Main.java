032e9a089d665ae805e3dc507d27f1cf09a6561c
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2021-06-17 12:50:37 +0200

    bump supported versions

--- bukkit/src/main/java/me/neznamy/tab/platforms/bukkit/Main.java
+++ bukkit/src/main/java/me/neznamy/tab/platforms/bukkit/Main.java
@@ -80,14 +80,14 @@ public class Main extends JavaPlugin {
 			if (SUPPORTED_VERSIONS.contains(serverPackage)) {
 				return true;
 			} else {
-				Bukkit.getConsoleSender().sendMessage("\u00a7c[TAB] This plugin version does not claim to support your server version. This jar has only been tested on 1.7.x - 1.16.5. Disabling.");
+				Bukkit.getConsoleSender().sendMessage("\u00a7c[TAB] No compatibility issue was found, but this plugin version does not claim to support your server version. This jar has only been tested on 1.5.x - 1.17. Disabling just to stay safe.");
 			}
 		} catch (Throwable e) {
 			if (SUPPORTED_VERSIONS.contains(serverPackage)) {
 				Bukkit.getConsoleSender().sendMessage("\u00a7c[TAB] Your server version is marked as compatible, but a compatibility issue was found. Please report the error below (include your server version & fork too)");
 				e.printStackTrace();
 			} else {
-				Bukkit.getConsoleSender().sendMessage("\u00a7c[TAB] Your server version is completely unsupported. This plugin version only supports 1.7.x - 1.16.5. Disabling.");
+				Bukkit.getConsoleSender().sendMessage("\u00a7c[TAB] Your server version is completely unsupported. This plugin version only supports 1.5.x - 1.17. Disabling.");
 			}
 		}
 		return false;
