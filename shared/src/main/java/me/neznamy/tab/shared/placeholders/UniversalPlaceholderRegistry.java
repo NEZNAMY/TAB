0021c7233c2a124bbc8f3131dbfdc31bb272988f
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2021-07-31 20:11:46 +0200

    fix placeholders used only in conditions not refreshing

--- shared/src/main/java/me/neznamy/tab/shared/placeholders/UniversalPlaceholderRegistry.java
+++ shared/src/main/java/me/neznamy/tab/shared/placeholders/UniversalPlaceholderRegistry.java
@@ -2,6 +2,7 @@ package me.neznamy.tab.shared.placeholders;
 
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -199,6 +200,12 @@ public class UniversalPlaceholderRegistry implements PlaceholderRegistry {
 			if (pm.getPlayerPlaceholderRefreshIntervals().containsKey(identifier)) {
 				refresh = pm.getPlayerPlaceholderRefreshIntervals().get(identifier);
 			}
+			//adding placeholders in conditions to the map so they are actually refreshed if not used anywhere else
+			List<String> placeholdersInConditions = new ArrayList<>();
+			for (String subcondition : list) {
+				placeholdersInConditions.addAll(pm.detectPlaceholders(subcondition));
+			}
+			pm.addUsedPlaceholders(placeholdersInConditions);
 			manager.registerPlayerPlaceholder(new PlayerPlaceholder(identifier, refresh) {
 
 				@Override
