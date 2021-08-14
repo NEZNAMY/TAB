b2ef957682ca744ffb6d3f067ed3e7a1346f43a5
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2021-08-14 12:03:47 +0200

    implement all data watcher registry values

--- bukkit/src/main/java/me/neznamy/tab/platforms/bukkit/nms/datawatcher/DataWatcherRegistry.java
+++ bukkit/src/main/java/me/neznamy/tab/platforms/bukkit/nms/datawatcher/DataWatcherRegistry.java
@@ -12,13 +12,28 @@ import me.neznamy.tab.platforms.bukkit.nms.NMSStorage;
  */
 public class DataWatcherRegistry {
 
-	//used registry types
 	private Object registryByte;
+	private Object registryShort;
 	private Object registryInteger;
 	private Object registryFloat;
 	private Object registryString;
-	private Object registryOptionalComponent;
+	private Object registryIChatBaseComponent;
+	private Object registryOptional_IChatBaseComponent;
+	@Deprecated
+	private Object registryOptional_ItemStack;
+	private Object registryItemStack;
+	private Object registryOptional_IBlockData;
 	private Object registryBoolean;
+	private Object registryParticleParam;
+	private Object registryVector3f;
+	private Object registryBlockPosition;
+	private Object registryOptional_BlockPosition;
+	private Object registryEnumDirection;
+	private Object registryOptional_UUID;
+	private Object registryNBTTagCompound;
+	private Object registryVillagerData;
+	private Object registryOptionalInt;
+	private Object registryEntityPose;
 
 	/**
 	 * Initializes required NMS classes and fields
@@ -30,11 +45,40 @@ public class DataWatcherRegistry {
 			registryInteger = fields.get("b");
 			registryFloat = fields.get("c");
 			registryString = fields.get("d");
+			registryIChatBaseComponent = fields.get("e");
 			if (nms.getMinorVersion() >= 13) {
-				registryOptionalComponent = fields.get("f");
+				registryOptional_IChatBaseComponent = fields.get("f");
+				registryItemStack = fields.get("g");
+				registryOptional_IBlockData = fields.get("h");
 				registryBoolean = fields.get("i");
+				registryParticleParam = fields.get("j");
+				registryVector3f = fields.get("k");
+				registryBlockPosition = fields.get("l");
+				registryOptional_BlockPosition = fields.get("m");
+				registryEnumDirection = fields.get("n");
+				registryOptional_UUID = fields.get("o");
+				registryNBTTagCompound = fields.get("p");
+				if (nms.getMinorVersion() >= 15) {
+					registryVillagerData = fields.get("q");
+					registryOptionalInt = fields.get("r");
+					registryEntityPose = fields.get("s");
+				}
 			} else {
+				registryOptional_IBlockData = fields.get("g");
 				registryBoolean = fields.get("h");
+				registryVector3f = fields.get("i");
+				registryBlockPosition = fields.get("j");
+				registryOptional_BlockPosition = fields.get("k");
+				registryEnumDirection = fields.get("l");
+				registryOptional_UUID = fields.get("m");
+				if (nms.getMinorVersion() >= 12) {
+					registryNBTTagCompound = fields.get("n");
+				}
+				if (nms.getMinorVersion() >= 11) {
+					registryItemStack = fields.get("f");
+				} else {
+					registryOptional_ItemStack = fields.get("f");
+				}
 			}
 		}
 	}
@@ -59,27 +103,87 @@ public class DataWatcherRegistry {
 		return fields;
 	}
 
-	public Object getBoolean() {
-		return registryBoolean;
-	}
-
 	public Object getByte() {
 		return registryByte;
 	}
-
-	public Object getFloat() {
-		return registryFloat;
+	
+	public Object getShort() {
+		return registryShort;
 	}
-
+	
 	public Object getInteger() {
 		return registryInteger;
 	}
-
+	
+	public Object getFloat() {
+		return registryFloat;
+	}
+	
 	public Object getString() {
 		return registryString;
 	}
-
+	
+	public Object getComponent() {
+		return registryIChatBaseComponent;
+	}
+	
 	public Object getOptionalComponent() {
-		return registryOptionalComponent;
+		return registryOptional_IChatBaseComponent;
+	}
+	
+	public Object getOptionalItemStack() {
+		return registryOptional_ItemStack;
+	}
+	
+	public Object getItemStack() {
+		return registryItemStack;
+	}
+	
+	public Object getOptionalIBlockData() {
+		return registryOptional_IBlockData;
+	}
+	
+	public Object getBoolean() {
+		return registryBoolean;
+	}
+	
+	public Object getParticleParam() {
+		return registryParticleParam;
+	}
+	
+	public Object getVector3f() {
+		return registryVector3f;
+	}
+	
+	public Object getBlockPosition() {
+		return registryBlockPosition;
+	}
+	
+	public Object getOptionalBlockPosition() {
+		return registryOptional_BlockPosition;
+	}
+	
+	public Object getEnumDirection() {
+		return registryEnumDirection;
+	}
+	
+	public Object getOptionalUUID() {
+		return registryOptional_UUID;
+	}
+	
+	public Object getNBTTagCompound() {
+		return registryNBTTagCompound;
+	}
+	
+	public Object getVillagerData() {
+		return registryVillagerData;
+	}
+	
+	public Object getOptionalInt() {
+		return registryOptionalInt;
+	}
+	
+	public Object getEntityPose() {
+		return registryEntityPose;
 	}
 }
