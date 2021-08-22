9e1a8c29ac7b6190c18c5865e82f798d1eec87fa
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2021-08-22 21:59:58 +0200

    remove redundant .toString() call

--- shared/src/main/java/me/neznamy/tab/shared/command/CpuCommand.java
+++ shared/src/main/java/me/neznamy/tab/shared/command/CpuCommand.java
@@ -122,7 +122,7 @@ public class CpuCommand extends SubCommand {
 			String core = String.format("&8&l%s &7%s &7(%s%%&7):", LINE_CHAR, entry.getKey(), colorize(decimal3.format(featureTotal), 5, 1));
 			List<String> messages = new ArrayList<>();
 			for (Entry<String, Float> type : entry.getValue().entrySet()){
-				messages.add("&3" + type.getKey().toString() + " - " + colorize(decimal3.format(type.getValue()), 5, 1) + "%");
+				messages.add("&3" + type.getKey() + " - " + colorize(decimal3.format(type.getValue()), 5, 1) + "%");
 			}
 			IChatBaseComponent message = new IChatBaseComponent(core.replace('&', '\u00a7'));
 			message.getModifier().onHoverShowText(new IChatBaseComponent(String.join("\n", messages).replace('&', '\u00a7')));
