9cc1112a3e98e9272945f15253ddbff5179ef63a
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2021-08-28 11:22:08 +0200

    use event listener for LP group refreshing instead of repeating task

--- shared/src/main/java/me/neznamy/tab/shared/features/GroupRefresher.java
+++ shared/src/main/java/me/neznamy/tab/shared/features/GroupRefresher.java
@@ -9,7 +9,11 @@ import me.neznamy.tab.api.TabPlayer;
 import me.neznamy.tab.shared.CpuConstants;
 import me.neznamy.tab.shared.ITabPlayer;
 import me.neznamy.tab.shared.TAB;
+import me.neznamy.tab.shared.permission.LuckPerms;
 import me.neznamy.tab.shared.permission.PermissionPlugin;
+import net.luckperms.api.LuckPermsProvider;
+import net.luckperms.api.event.EventSubscription;
+import net.luckperms.api.event.user.UserDataRecalculateEvent;
 
 /**
  * Permission group refresher
@@ -18,6 +22,7 @@ public class GroupRefresher extends TabFeature {
 
 	public static final String DEFAULT_GROUP = "NONE";
 	
+	private Object luckPermsSub;
 	private PermissionPlugin plugin;
 	private boolean groupsByPermissions;
 	private List<String> primaryGroupFindingList;
@@ -30,13 +35,23 @@ public class GroupRefresher extends TabFeature {
 		for (Object group : TAB.getInstance().getConfiguration().getConfig().getStringList("primary-group-finding-list", Arrays.asList("Owner", "Admin", "Helper", "default"))){
 			primaryGroupFindingList.add(group.toString());
 		}
-		TAB.getInstance().getCPUManager().startRepeatingMeasuredTask(1000, "refreshing permission groups", this, CpuConstants.UsageCategory.REFRESHING_GROUPS, () -> {
-			for (TabPlayer p : TAB.getInstance().getOnlinePlayers()) refreshPlayer(p);
-		});
+		if (plugin instanceof LuckPerms) {
+			luckPermsSub = LuckPermsProvider.get().getEventBus().subscribe(UserDataRecalculateEvent.class, (event) -> refreshPlayer(TAB.getInstance().getPlayer(event.getUser().getUniqueId())));
+		} else {
+			TAB.getInstance().getCPUManager().startRepeatingMeasuredTask(1000, "refreshing permission groups", this, CpuConstants.UsageCategory.REFRESHING_GROUPS, () -> {
+				for (TabPlayer p : TAB.getInstance().getOnlinePlayers()) refreshPlayer(p);
+			});
+		}
 	}
 	
 	private void refreshPlayer(TabPlayer p) {
 		((ITabPlayer)p).setGroup(detectPermissionGroup(p), true);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public void unload() {
+		if (luckPermsSub != null) ((EventSubscription<UserDataRecalculateEvent>)luckPermsSub).close();
 	}
 
 	public String detectPermissionGroup(TabPlayer p) {
