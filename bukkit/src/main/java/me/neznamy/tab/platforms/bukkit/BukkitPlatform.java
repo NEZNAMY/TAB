addc3f322f1767cdca8737e7c86c615e7c7890a6
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2021-11-11 10:40:56 +0100

    load feature after registering placeholders correctly to avoid wrong registration

--- bukkit/src/main/java/me/neznamy/tab/platforms/bukkit/BukkitPlatform.java
+++ bukkit/src/main/java/me/neznamy/tab/platforms/bukkit/BukkitPlatform.java
@@ -104,9 +104,9 @@ public class BukkitPlatform implements Platform {
 		essentials = Bukkit.getPluginManager().getPlugin("Essentials");
 		TAB tab = TAB.getInstance();
 		if (tab.getConfiguration().isPipelineInjection()) tab.getFeatureManager().registerFeature("injection", new BukkitPipelineInjector(nms));
-		loadNametagFeature(tab);
 		new BukkitPlaceholderRegistry().registerPlaceholders(tab.getPlaceholderManager());
 		new UniversalPlaceholderRegistry().registerPlaceholders(tab.getPlaceholderManager());
+		loadNametagFeature(tab);
 		tab.loadUniversalFeatures();
 		if (tab.getConfiguration().getConfig().getBoolean("bossbar.enabled", false)) {
 			if (nms.getMinorVersion() < 9) {
