4f165790639128508fb3cd0c1431bcfda37a2aa3
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2021-12-02 11:01:19 +0100

    support mineskin by uuid

--- shared/src/main/java/me/neznamy/tab/shared/features/layout/skin/MineSkin.java
+++ shared/src/main/java/me/neznamy/tab/shared/features/layout/skin/MineSkin.java
@@ -21,7 +21,14 @@ public class MineSkin extends SkinSource {
 	@Override
 	public List<String> download(String input) {
 		try {
-			JSONObject json = getResponse("https://api.mineskin.org/get/id/" + input);
+			String type;
+			try {
+				Integer.parseInt(input);
+				type = input.length() < 20 ? "id" : "uuid";
+			} catch (NumberFormatException ex) {
+				type = "uuid";
+			}
+			JSONObject json = getResponse("https://api.mineskin.org/get/" + type + "/" + input);
 			JSONObject data = (JSONObject) json.get("data");
 			JSONObject texture = (JSONObject) data.get("texture");
 			String value = (String) texture.get("value");
