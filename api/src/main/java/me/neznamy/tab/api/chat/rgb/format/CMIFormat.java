5f8dd869e0faebce12a87f6c604217201c764fe6
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2021-12-11 15:58:45 +0100

    remove redundant escape in RGB patterns

--- api/src/main/java/me/neznamy/tab/api/chat/rgb/format/CMIFormat.java
+++ api/src/main/java/me/neznamy/tab/api/chat/rgb/format/CMIFormat.java
@@ -8,7 +8,7 @@ import java.util.regex.Pattern;
  */
 public class CMIFormat implements RGBFormatter {
 
-	private final Pattern pattern = Pattern.compile("\\{#[0-9a-fA-F]{6}\\}");
+	private final Pattern pattern = Pattern.compile("\\{#[0-9a-fA-F]{6}}");
 	
 	@Override
 	public String reformat(String text) {
