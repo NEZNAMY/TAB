0c874ad56dc98ab002d910db2f07bdafe84ebaab
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2022-01-20 13:51:24 +0100

    only keep lowercase entries in sorting map

--- shared/src/main/java/me/neznamy/tab/shared/features/sorting/types/Placeholder.java
+++ shared/src/main/java/me/neznamy/tab/shared/features/sorting/types/Placeholder.java
@@ -1,6 +1,7 @@
 package me.neznamy.tab.shared.features.sorting.types;
 
 import java.util.LinkedHashMap;
+import java.util.Locale;
 
 import me.neznamy.tab.api.chat.EnumChatFormat;
 import me.neznamy.tab.shared.ITabPlayer;
@@ -31,13 +32,13 @@ public class Placeholder extends SortingType {
 	public String getChars(ITabPlayer p) {
 		String output = EnumChatFormat.color(setPlaceholders(p));
 		p.setTeamNameNote(p.getTeamNameNote() + sortingPlaceholder + " returned \"" + output + "\"");
-		String sortingValue = sortingMap.get(output);
+		String sortingValue = sortingMap.get(output.toLowerCase(Locale.US));
 		if (sortingValue == null) {
 			sortingValue = String.valueOf(sortingMap.size()+1);
 			TAB.getInstance().getErrorManager().oneTimeConsoleError("Sorting by predefined placeholder values is enabled, but output \"" + output + "\" is not listed. List: " + sortingMap.keySet());
 			p.setTeamNameNote(p.getTeamNameNote() + "&c (not in list)&r. ");
 		} else {
-			p.setTeamNameNote(p.getTeamNameNote() + " (#" + Integer.parseInt(sortingMap.get(output)) + " in list). &r");
+			p.setTeamNameNote(p.getTeamNameNote() + "&r (#" + Integer.parseInt(sortingMap.get(output)) + " in list). &r");
 		}
 		return sortingValue;
 	}
