d7636a05f40b0924d8dfb7ec4c90ed527afce4c1
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2022-02-13 23:05:47 +0100

    drastically reduce amount of permission checks by the feature

--- shared/src/main/java/me/neznamy/tab/shared/features/SpectatorFix.java
+++ shared/src/main/java/me/neznamy/tab/shared/features/SpectatorFix.java
@@ -48,25 +48,23 @@ public class SpectatorFix extends TabFeature {
 	
 	@Override
 	public void onPlayerInfo(TabPlayer receiver, PacketPlayOutPlayerInfo info) {
-		if (receiver.hasPermission(TabConstants.Permission.SPECTATOR_BYPASS)) return;
 		if (info.getAction() != EnumPlayerInfoAction.UPDATE_GAME_MODE && info.getAction() != EnumPlayerInfoAction.ADD_PLAYER) return;
 		for (PlayerInfoData playerInfoData : info.getEntries()) {
-			if (playerInfoData.getGameMode() == EnumGamemode.SPECTATOR) {
-				TabPlayer changed = TAB.getInstance().getPlayerByTabListUUID(playerInfoData.getUniqueId());
-				if (changed != receiver) playerInfoData.setGameMode(EnumGamemode.CREATIVE);
-			}
+			if (playerInfoData.getGameMode() != EnumGamemode.SPECTATOR) continue;
+			if (receiver.hasPermission(TabConstants.Permission.SPECTATOR_BYPASS)) continue;
+			TabPlayer changed = TAB.getInstance().getPlayerByTabListUUID(playerInfoData.getUniqueId());
+			if (changed != receiver) playerInfoData.setGameMode(EnumGamemode.CREATIVE);
 		}
 	}
 	
 	@Override
 	public void onJoin(TabPlayer p) {
-		if (p.hasPermission(TabConstants.Permission.SPECTATOR_BYPASS)) return;
 		List<PlayerInfoData> list = new ArrayList<>();
 		for (TabPlayer target : TAB.getInstance().getOnlinePlayers()) {
 			if (p == target || target.getGamemode() != 3) continue;
 			list.add(new PlayerInfoData(target.getUniqueId(), EnumGamemode.CREATIVE));
 		}
-		if (list.isEmpty()) return;
+		if (list.isEmpty() || p.hasPermission(TabConstants.Permission.SPECTATOR_BYPASS)) return;
 		p.sendCustomPacket(new PacketPlayOutPlayerInfo(EnumPlayerInfoAction.UPDATE_GAME_MODE, list), this);
 	}
 
