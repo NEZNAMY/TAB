1221509d240fe0a24790c2178728eb91c8bea11a
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2022-03-31 20:52:12 +0200

    MySQL - Do not lowercase _DEFAULT_ group name, which caused it to not load (#546) (for real this time)

--- shared/src/main/java/me/neznamy/tab/shared/config/mysql/MySQLGroupConfiguration.java
+++ shared/src/main/java/me/neznamy/tab/shared/config/mysql/MySQLGroupConfiguration.java
@@ -22,7 +22,8 @@ public class MySQLGroupConfiguration implements PropertyConfiguration {
         mysql.execute("create table if not exists tab_groups (`group` varchar(64), `property` varchar(16), `value` varchar(1024), world varchar(64), server varchar(64))");
         CachedRowSet crs = mysql.getCRS("select * from tab_groups");
         while (crs.next()) {
-            String group = crs.getString("group").toLowerCase(Locale.US);
+            String group = crs.getString("group");
+            if (!group.equals(DEFAULT_GROUP)) group = group.toLowerCase(Locale.US);
             String property = crs.getString("property");
             String value = crs.getString("value");
             String world = crs.getString("world");
@@ -62,22 +63,22 @@ public class MySQLGroupConfiguration implements PropertyConfiguration {
 
     @Override
     public String[] getProperty(String group, String property, String server, String world) {
-        group = group.toLowerCase(Locale.US);
+        String lowercaseGroup = group.equals(DEFAULT_GROUP) ? group : group.toLowerCase(Locale.US);
         Object value;
-        if ((value = perWorld.getOrDefault(world, new HashMap<>()).getOrDefault(group, new HashMap<>()).get(property)) != null) {
-            return new String[] {toString(value), String.format("group=%s,world=%s", group, world)};
+        if ((value = perWorld.getOrDefault(world, new HashMap<>()).getOrDefault(lowercaseGroup, new HashMap<>()).get(property)) != null) {
+            return new String[] {toString(value), String.format("group=%s,world=%s", lowercaseGroup, world)};
         }
         if ((value = perWorld.getOrDefault(world, new HashMap<>()).getOrDefault(DEFAULT_GROUP, new HashMap<>()).get(property)) != null) {
             return new String[] {toString(value), String.format("group=%s,world=%s", DEFAULT_GROUP, world)};
         }
-        if ((value = perServer.getOrDefault(server, new HashMap<>()).getOrDefault(group, new HashMap<>()).get(property)) != null) {
-            return new String[] {toString(value), String.format("group=%s,server=%s", group, server)};
+        if ((value = perServer.getOrDefault(server, new HashMap<>()).getOrDefault(lowercaseGroup, new HashMap<>()).get(property)) != null) {
+            return new String[] {toString(value), String.format("group=%s,server=%s", lowercaseGroup, server)};
         }
         if ((value = perServer.getOrDefault(server, new HashMap<>()).getOrDefault(DEFAULT_GROUP, new HashMap<>()).get(property)) != null) {
             return new String[] {toString(value), String.format("group=%s,server=%s", DEFAULT_GROUP, server)};
         }
-        if ((value = values.getOrDefault(group, new HashMap<>()).get(property)) != null) {
-            return new String[] {toString(value), String.format("group=%s", group)};
+        if ((value = values.getOrDefault(lowercaseGroup, new HashMap<>()).get(property)) != null) {
+            return new String[] {toString(value), String.format("group=%s", lowercaseGroup)};
         }
         if ((value = values.getOrDefault(DEFAULT_GROUP, new HashMap<>()).get(property)) != null) {
             return new String[] {toString(value), String.format("group=%s", DEFAULT_GROUP)};
