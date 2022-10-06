28f05fc2af59ed583adff93f0defb653c52daa33
Callum Seabrook <callum.seabrook@prevarinite.com>
2022-10-06 12:02:28 +0100

    Changed publishing artifact IDs to begin with "tab-"

--- build-logic/src/main/kotlin/tab.standard-conventions.gradle.kts
+++ build-logic/src/main/kotlin/tab.standard-conventions.gradle.kts
@@ -6,4 +6,7 @@ plugins {
 indra {
     publishReleasesTo("krypton", "https://repo.kryptonmc.org/releases")
     publishSnapshotsTo("krypton", "https://repo.kryptonmc.org/snapshots")
+    configurePublications {
+        artifactId = "tab-$artifactId"
+    }
 }
