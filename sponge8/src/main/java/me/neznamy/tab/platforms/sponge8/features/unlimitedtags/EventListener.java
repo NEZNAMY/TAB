e2dda540115ca5492960b0e2a7d2a5b8455aaf89
Callum Seabrook <callum.seabrook@prevarinite.com>
2023-02-15 11:43:05 +0000

    Added sneaking support for unlimited nametags Sponge

--- sponge8/src/main/java/me/neznamy/tab/platforms/sponge8/features/unlimitedtags/EventListener.java
+++ sponge8/src/main/java/me/neznamy/tab/platforms/sponge8/features/unlimitedtags/EventListener.java
@@ -4,7 +4,11 @@ import lombok.RequiredArgsConstructor;
 import me.neznamy.tab.api.TabAPI;
 import me.neznamy.tab.api.TabConstants;
 import me.neznamy.tab.api.TabPlayer;
+import org.spongepowered.api.data.Keys;
+import org.spongepowered.api.data.value.Value;
+import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.data.ChangeDataHolderEvent;
 import org.spongepowered.api.event.entity.living.player.RespawnPlayerEvent;
 
 @RequiredArgsConstructor
@@ -18,6 +22,23 @@ public class EventListener {
             TabPlayer respawned = TabAPI.getInstance().getPlayer(event.entity().uniqueId());
             if (feature.isPlayerDisabled(respawned)) return;
             feature.getArmorStandManager(respawned).teleport();
+        });
+    }
+
+    @Listener
+    public void onSneak(final ChangeDataHolderEvent.ValueChange event) {
+        if (!(event.targetHolder() instanceof Player)) return;
+        final Player player = (Player) event.targetHolder();
+
+        event.originalChanges().ifSuccessful(changes -> {
+            for (final Value.Immutable<?> change : changes) {
+                if (change.key() != Keys.IS_SNEAKING) continue;
+                final boolean value = ((Value.Immutable<Boolean>) change).get();
+
+                TabPlayer p = TabAPI.getInstance().getPlayer(player.uniqueId());
+                if (p == null || feature.isPlayerDisabled(p)) return;
+                TabAPI.getInstance().getThreadManager().runMeasuredTask(feature, TabConstants.CpuUsageCategory.PLAYER_SNEAK, () -> feature.getArmorStandManager(p).sneak(value));
+            }
         });
     }
 }
