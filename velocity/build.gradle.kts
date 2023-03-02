5ce59ce44fb5e26f4b8e7752a60df8f84b9799f9
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2023-03-02 20:33:37 +0100

    Fix compilation

--- velocity/build.gradle.kts
+++ velocity/build.gradle.kts
@@ -1,6 +1,8 @@
 dependencies {
     implementation(projects.shared)
     implementation(libs.bstats.velocity)
-    compileOnly(libs.velocity)
+    compileOnly(libs.velocity) {
+        exclude("com.velocitypowered", "velocity-brigadier") // Breaks compilation due to requiring Java 11
+    }
     annotationProcessor(libs.velocity)
 }
