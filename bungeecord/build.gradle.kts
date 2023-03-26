c1219195c7c6a54257d10be3b9f90dfd02d50215
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2023-03-26 17:06:57 +0200

    Remove workarounds for fixing compilation, which are no longer needed

--- bungeecord/build.gradle.kts
+++ bungeecord/build.gradle.kts
@@ -3,5 +3,4 @@ dependencies {
     implementation(libs.bstats.bungeecord)
     compileOnly(libs.bungeecord)
     compileOnly(libs.redisBungee)
-    compileOnly("io.netty:netty-transport-native-epoll:4.1.84.Final") // Fixing compilation with 1.19.4 update
 }
