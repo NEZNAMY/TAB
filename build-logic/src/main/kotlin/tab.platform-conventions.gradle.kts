e54d342ac5839a7510aab071cc39463c1cfb5939
Callum Seabrook <callum.seabrook@prevarinite.com>
2023-04-04 21:30:52 +0100

    Made relocations have a consistent pattern

--- build-logic/src/main/kotlin/tab.platform-conventions.gradle.kts
+++ build-logic/src/main/kotlin/tab.platform-conventions.gradle.kts
@@ -9,7 +9,7 @@ tasks {
         relocate("org.bstats", "me.neznamy.tab.libs.org.bstats")
         relocate("org.json.simple", "me.neznamy.tab.libs.org.json.simple")
         relocate("net.kyori.event", "me.neznamy.tab.libs.net.kyori.event")
-        relocate("me.neznamy.yamlassist", "me.neznamy.tab.libs.yamlassist")
-        relocate("org.yaml.snakeyaml", "me.neznamy.tab.libs.snakeyaml")
+        relocate("me.neznamy.yamlassist", "me.neznamy.tab.libs.me.neznamy.yamlassist")
+        relocate("org.yaml.snakeyaml", "me.neznamy.tab.libs.org.yaml.snakeyaml")
     }
 }
