ba6826f3d1820f3c44c529fffb3164ee2d8cc6dd
Callum Seabrook <callum.seabrook@prevarinite.com>
2023-04-05 12:19:59 +0100

    Added variable to change target Minecraft version for Fabric

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -10,15 +10,19 @@ repositories {
     maven("https://oss.sonatype.org/content/repositories/snapshots")
 }
 
+// Supported versions: 1.19.3, 1.19.4
+val minecraftVersion = "1.19.4"
+
 dependencies {
     implementation(projects.shared)
 
-    minecraft("com.mojang", "minecraft", "1.19.4")
+    minecraft("com.mojang", "minecraft", minecraftVersion)
     mappings(loom.officialMojangMappings())
+
     modImplementation("net.fabricmc", "fabric-loader", "0.14.17")
     modImplementation("me.lucko", "fabric-permissions-api", "0.2-SNAPSHOT")
     val apiModules = setOf("fabric-api-base", "fabric-command-api-v2", "fabric-lifecycle-events-v1", "fabric-networking-api-v1")
-    apiModules.forEach { modImplementation(fabricApi.module(it, "0.73.0+1.19.4")) }
+    apiModules.forEach { modImplementation(fabricApi.module(it, "0.73.0+$minecraftVersion")) }
 }
 
 tasks.compileJava {
