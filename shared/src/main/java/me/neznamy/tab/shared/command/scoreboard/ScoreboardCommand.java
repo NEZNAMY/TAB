e51f4bb3a178bfaba22a803f3c2eca95d706abd3
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2023-06-12 13:34:30 +0200

    Remove unused methods

--- shared/src/main/java/me/neznamy/tab/shared/command/scoreboard/ScoreboardCommand.java
+++ shared/src/main/java/me/neznamy/tab/shared/command/scoreboard/ScoreboardCommand.java
@@ -1,9 +1,6 @@
 package me.neznamy.tab.shared.command.scoreboard;
 
-import me.neznamy.tab.shared.TAB;
-import me.neznamy.tab.shared.TabConstants;
 import me.neznamy.tab.shared.command.SubCommand;
-import me.neznamy.tab.shared.features.scoreboard.ScoreboardManagerImpl;
 import me.neznamy.tab.shared.platform.TabPlayer;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -34,7 +31,11 @@ public class ScoreboardCommand extends SubCommand {
             command = getSubcommands().get(args[0].toLowerCase());
         } else {
             command = getSubcommands().get("toggle");
-            args = new String[]{""};
+            if (command.hasPermission(sender)) {
+                command.execute(sender, new String[0]);
+            } else {
+                sendMessage(sender, getMessages().getNoPermission());
+            }
         }
         if (command != null) {
             if (command.hasPermission(sender)) {
@@ -45,39 +46,5 @@ public class ScoreboardCommand extends SubCommand {
         } else {
             sendMessages(sender, getMessages().getScoreboardHelpMenu());
         }
-    }
-
-    private void toggle(@Nullable TabPlayer sender, @NotNull ScoreboardManagerImpl scoreboard) {
-        if (sender == null) {
-            sendMessage(null, getMessages().getCommandOnlyFromGame());
-            return;
-        }
-        if (sender.hasPermission(TabConstants.Permission.COMMAND_SCOREBOARD_TOGGLE)) {
-            if (scoreboard.getOtherPluginScoreboards().containsKey(sender)) return; //not overriding other plugins
-            scoreboard.toggleScoreboard(sender, true);
-        } else {
-            sender.sendMessage(getMessages().getNoPermission(), true);
-        }
-    }
-
-    private TabPlayer getTarget(@Nullable TabPlayer sender, @NotNull String[] args) {
-        if (args.length >= 2 && TAB.getInstance().getPlayer(args[1]) != null) {
-            if (hasPermission(sender, TabConstants.Permission.COMMAND_SCOREBOARD_TOGGLE_OTHER)) {
-                return TAB.getInstance().getPlayer(args[1]);
-            } else {
-                sendMessage(sender, getMessages().getNoPermission());
-            }
-        } else {
-            if (hasPermission(sender, TabConstants.Permission.COMMAND_SCOREBOARD_TOGGLE)) {
-                return sender;
-            } else {
-                sendMessage(sender, getMessages().getNoPermission());
-            }
-        }
-        return null;
-    }
-
-    private @Nullable ScoreboardManagerImpl getScoreboardManager() {
-        return TAB.getInstance().getFeatureManager().getFeature(TabConstants.Feature.SCOREBOARD);
     }
 }
