2787b1735b6f4bfcbfd437cd86bdcfc77a0591cf
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2023-08-04 22:07:06 +0200

    Header/Footer - Fix being sent on server switch even if disable condition is met & fix being sent twice

--- shared/src/main/java/me/neznamy/tab/shared/features/HeaderFooter.java
+++ shared/src/main/java/me/neznamy/tab/shared/features/HeaderFooter.java
@@ -58,9 +58,10 @@ public class HeaderFooter extends TabFeature implements HeaderFooterManager, Joi
 
     @Override
     public void onServerChange(@NotNull TabPlayer p, @NotNull String from, @NotNull String to) {
-        updateProperties(p);
-        // Velocity clears header/footer on server switch, which can be a problem without placeholders that change often
-        // Resend immediately instead of the next time a placeholder changes value
+        // Velocity clears header/footer on server switch, resend regardless of whether values changed or not
+        p.setProperty(this, TabConstants.Property.HEADER, getProperty(p, TabConstants.Property.HEADER));
+        p.setProperty(this, TabConstants.Property.FOOTER, getProperty(p, TabConstants.Property.FOOTER));
+        if (disableChecker.isDisabledPlayer(p)) return;
         sendHeaderFooter(p, p.getProperty(TabConstants.Property.HEADER).get(), p.getProperty(TabConstants.Property.FOOTER).get());
     }
 
