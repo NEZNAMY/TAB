f9be9d4c4b869a3694219a42e5f15da2c578638f
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2023-09-07 18:09:38 +0200

    Improve check to reduce errors thrown

--- shared/src/main/java/me/neznamy/tab/shared/chat/rgb/format/MiniMessageFormat.java
+++ shared/src/main/java/me/neznamy/tab/shared/chat/rgb/format/MiniMessageFormat.java
@@ -1,5 +1,6 @@
 package me.neznamy.tab.shared.chat.rgb.format;
 
+import me.neznamy.tab.shared.chat.EnumChatFormat;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.jetbrains.annotations.NotNull;
@@ -11,7 +12,10 @@ public class MiniMessageFormat implements RGBFormatter {
 
     @Override
     public @NotNull String reformat(@NotNull String text) {
-        if (!text.contains("<")) return text;
+        if (
+                !text.contains("<") // No MiniMessage syntax is used
+                || text.contains(EnumChatFormat.COLOR_STRING) // MiniMessage throws errors when using legacy color code
+        ) return text;
         try {
             return LegacyComponentSerializer.legacySection().serialize(MiniMessage.miniMessage().deserialize(text));
         } catch (Throwable ignored) {
