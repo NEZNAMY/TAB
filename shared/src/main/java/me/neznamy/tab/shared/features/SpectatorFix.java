c5420ead90bef195198ca92aab704aca93f524cb
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2023-09-13 16:55:21 +0200

    Send gamemode update packet on world switch when spectator fix is enabled as the server may resend gamemode on world switch back to spectator (#1069)

--- shared/src/main/java/me/neznamy/tab/shared/features/SpectatorFix.java
+++ shared/src/main/java/me/neznamy/tab/shared/features/SpectatorFix.java
@@ -12,7 +12,8 @@ import org.jetbrains.annotations.NotNull;
  * the bottom of TabList with transparent name. Does not work on self as that would result
  * in players not being able to clip through walls.
  */
-public class SpectatorFix extends TabFeature implements JoinListener, GameModeListener, Loadable, UnLoadable, ServerSwitchListener {
+public class SpectatorFix extends TabFeature implements JoinListener, GameModeListener, Loadable, UnLoadable,
+        ServerSwitchListener, WorldSwitchListener {
 
     @Getter private final String featureName = "Spectator fix";
 
@@ -78,5 +79,14 @@ public class SpectatorFix extends TabFeature implements JoinListener, GameModeLi
                 updatePlayer(all, false, true);
             }
         });
+    }
+
+    @Override
+    public void onWorldChange(@NotNull TabPlayer changed, @NotNull String from, @NotNull String to) {
+        // Some server versions may resend gamemode on world switch, resend false value again
+        for (TabPlayer viewer : TAB.getInstance().getOnlinePlayers()) {
+            if (viewer == changed || changed.getGamemode() != 3 || viewer.hasPermission(TabConstants.Permission.SPECTATOR_BYPASS)) continue;
+            viewer.getTabList().updateGameMode(changed.getTablistId(), 0);
+        }
     }
 }
