e67d93a5924f4c78becbbe7fd4f769d6ebaad06c
Galen Huang <74390539+GsTio86@users.noreply.github.com>
2023-09-23 16:58:19 +0800

    Update DataWatcher.java

--- bukkit/src/main/java/me/neznamy/tab/platforms/bukkit/nms/DataWatcher.java
+++ bukkit/src/main/java/me/neznamy/tab/platforms/bukkit/nms/DataWatcher.java
@@ -107,11 +107,7 @@ public class DataWatcher implements EntityData {
             }
         }
         if (BukkitReflection.is1_19_3Plus()) {
-            try {
-                DataWatcher_markDirty = ReflectionUtils.getOnlyMethod(DataWatcher, void.class, DataWatcherObject);
-            } catch (IllegalStateException e) {
-                DataWatcher_markDirty = DataWatcher.getMethod("markDirty", DataWatcherObject);
-            }
+            DataWatcher_markDirty = ReflectionUtils.getMethods(DataWatcher, void.class, DataWatcherObject).get(0);
         }
         if (minorVersion >= 9) {
             newDataWatcherObject = DataWatcherObject.getConstructor(int.class, DataWatcherSerializer);
