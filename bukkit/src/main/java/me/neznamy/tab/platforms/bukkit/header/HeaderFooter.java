155886e0009edd067975c977c7c21f3e5654ddd0
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2023-11-04 15:39:45 +0100

    Fix typo

--- bukkit/src/main/java/me/neznamy/tab/platforms/bukkit/header/HeaderFooter.java
+++ bukkit/src/main/java/me/neznamy/tab/platforms/bukkit/header/HeaderFooter.java
@@ -32,12 +32,12 @@ public abstract class HeaderFooter {
             Bukkit.getConsoleSender().sendMessage(EnumChatFormat.RED.getFormat() + "[TAB] Failed to initialize NMS fields for " +
                     "sending Header/Footer due to a compatibility error. Using fallback solution using Bukkit API. " +
                     "This will drastically drop performance, as well as miss support for fonts (added in 1.16). " +
-                    "Please update the plugin with to version proper support for your server version.");
+                    "Please update the plugin to version with proper support for your server version.");
         } else if (BukkitReflection.getMinorVersion() >= 8) {
             Bukkit.getConsoleSender().sendMessage(EnumChatFormat.RED.getFormat() + "[TAB] Failed to initialize NMS fields for " +
                     "sending Header/Footer due to a compatibility error. No fallback solution was found either. This will " +
                     "result in the feature not working. " +
-                    "Please update the plugin with to version proper support for your server version.");
+                    "Please update the plugin to version with proper support for your server version.");
         }
     }
 
