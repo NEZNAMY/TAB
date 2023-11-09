f47b8b5528e15c9c4642587208b852c5b817da20
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2023-11-09 18:30:56 +0100

    Alternate implementation of #1136

--- velocity/build.gradle.kts
+++ velocity/build.gradle.kts
@@ -4,6 +4,7 @@ dependencies {
     compileOnly("com.github.limework.redisbungee:RedisBungee-Velocity:0.11.0")
     compileOnly("com.velocitypowered:velocity-api:3.1.2-SNAPSHOT")
     annotationProcessor("com.velocitypowered:velocity-api:3.1.2-SNAPSHOT")
+    compileOnly("com.github.LeonMangler:PremiumVanishAPI:2.9.0-4")
 }
 
 tasks.compileJava {
