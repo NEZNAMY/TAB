557970ba4e5b3904edb01d322a53c1d3230df70d
NicoNekoDev <nicuch100@gmail.com>
2023-12-10 11:18:41 +0200

    Add NexEngine gradient
    https://github.com/nulli0n/NexEngine-spigot/wiki/Configuration-Tips

--- shared/src/main/java/me/neznamy/tab/shared/chat/rgb/RGBUtils.java
+++ shared/src/main/java/me/neznamy/tab/shared/chat/rgb/RGBUtils.java
@@ -11,6 +11,7 @@ import me.neznamy.tab.shared.chat.rgb.gradient.CMIGradient;
 import me.neznamy.tab.shared.chat.rgb.gradient.CommonGradient;
 import me.neznamy.tab.shared.chat.rgb.gradient.GradientPattern;
 import me.neznamy.tab.shared.chat.rgb.gradient.KyoriGradient;
+import me.neznamy.tab.shared.chat.rgb.gradient.NexEngineGradient;
 import me.neznamy.tab.shared.util.ReflectionUtils;
 
 import java.util.ArrayList;
@@ -60,7 +61,8 @@ public class RGBUtils {
                 new CommonGradient(Pattern.compile("<\\$#[0-9a-fA-F]{6}>[^<]*<\\$#[0-9a-fA-F]{6}>"),
                         Pattern.compile("<\\$#[0-9a-fA-F]{6}\\|.>[^<]*<\\$#[0-9a-fA-F]{6}>"),
                         "<$", 10, 3, 10, 7),
-                new KyoriGradient()
+                new KyoriGradient(),
+                new NexEngineGradient()
         };
     }
 
---
+++ shared/src/main/java/me/neznamy/tab/shared/chat/rgb/gradient/NexEngineGradient.java
@@ -0,0 +1,27 @@
+package me.neznamy.tab.shared.chat.rgb.gradient;
+
+import me.neznamy.tab.shared.chat.TextColor;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class NexEngineGradient implements GradientPattern {
+
+    //pattern for {gradient:#RRGGBB}
+    private final Pattern pattern = Pattern.compile("<gradient:#([A-Fa-f0-9]{6})>(.*?)</gradient:#([A-Fa-f0-9]{6})>");
+
+    @Override
+    public String applyPattern(@NotNull String text, boolean ignorePlaceholders) {
+        if (!text.contains("<grad")) return text;
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            TextColor start = new TextColor(matcher.group(1));
+            String content = matcher.group(2);
+            TextColor end = new TextColor(matcher.group(3));
+            String applied = asGradient(start, content, end);
+            text = text.replace(text, applied);
+        }
+        return text;
+    }
+}
