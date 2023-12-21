ae48041a96e80d8d5e41145cfc212806b745ad4a
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2023-12-21 23:55:57 +0100

    Simplify

--- shared/src/main/java/me/neznamy/tab/shared/features/HeaderFooter.java
+++ shared/src/main/java/me/neznamy/tab/shared/features/HeaderFooter.java
@@ -59,9 +59,7 @@ public class HeaderFooter extends TabFeature implements HeaderFooterManager, Joi
     @Override
     public void onServerChange(@NotNull TabPlayer p, @NotNull String from, @NotNull String to) {
         // Velocity clears header/footer on server switch, resend regardless of whether values changed or not
-        p.setProperty(this, TabConstants.Property.HEADER, getProperty(p, TabConstants.Property.HEADER));
-        p.setProperty(this, TabConstants.Property.FOOTER, getProperty(p, TabConstants.Property.FOOTER));
-        sendHeaderFooter(p, p.getProperty(TabConstants.Property.HEADER).get(), p.getProperty(TabConstants.Property.FOOTER).get());
+        refresh(p, true);
     }
 
     @Override
