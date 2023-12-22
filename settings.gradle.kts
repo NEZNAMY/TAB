8e27cba0f63d18d42bd5d46fc460012b6cfb1221
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2023-12-22 15:30:05 +0100

    Fix compilation

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 dependencyResolutionManagement {
     repositories {
         mavenCentral() // Netty, SnakeYaml, json-simple, Guava, Kyori event, bStats, AuthLib, LuckPerms
+        maven("https://repo.papermc.io/repository/maven-public/") // Velocity
         maven("https://repo.extendedclip.com/content/repositories/placeholderapi/") // PlaceholderAPI
         maven("https://repo.viaversion.com/") // ViaVersion
         maven("https://repo.opencollab.dev/maven-snapshots/") // Floodgate
