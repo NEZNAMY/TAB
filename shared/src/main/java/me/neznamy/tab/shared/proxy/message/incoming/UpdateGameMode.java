cb4d96f1ba674703de8bbec24d516e6772222736
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2023-12-29 22:15:05 +0100

    Remove unused imports

--- shared/src/main/java/me/neznamy/tab/shared/proxy/message/incoming/UpdateGameMode.java
+++ shared/src/main/java/me/neznamy/tab/shared/proxy/message/incoming/UpdateGameMode.java
@@ -1,13 +1,8 @@
 package me.neznamy.tab.shared.proxy.message.incoming;
 
 import com.google.common.io.ByteArrayDataInput;
-import me.neznamy.tab.shared.TAB;
-import me.neznamy.tab.shared.platform.TabPlayer;
 import me.neznamy.tab.shared.proxy.ProxyTabPlayer;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class UpdateGameMode implements IncomingMessage {
 
