016af4bfee4157f626e1a193eb8fe04f95dd3686
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2024-01-06 20:54:23 +0100

    Make DummyEntityView methods do nothing instead of throwing

--- shared/src/main/java/me/neznamy/tab/shared/backend/entityview/DummyEntityView.java
+++ shared/src/main/java/me/neznamy/tab/shared/backend/entityview/DummyEntityView.java
@@ -4,82 +4,83 @@ import me.neznamy.tab.shared.backend.EntityData;
 import me.neznamy.tab.shared.backend.Location;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collections;
 import java.util.UUID;
 
 public class DummyEntityView implements EntityView {
 
     @Override
     public void spawnEntity(int entityId, @NotNull UUID id, @NotNull Object entityType, @NotNull Location location, @NotNull EntityData data) {
-        throw new UnsupportedOperationException("Not available on this platform");
+        // Do nothing
     }
 
     @Override
     public void updateEntityMetadata(int entityId, @NotNull EntityData data) {
-        throw new UnsupportedOperationException("Not available on this platform");
+        // Do nothing
     }
 
     @Override
     public void teleportEntity(int entityId, @NotNull Location location) {
-        throw new UnsupportedOperationException("Not available on this platform");
+        // Do nothing
     }
 
     @Override
     public void destroyEntities(int... entities) {
-        throw new UnsupportedOperationException("Not available on this platform");
+        // Do nothing
     }
 
     @Override
     public boolean isDestroyPacket(Object packet) {
-        throw new UnsupportedOperationException("Not available on this platform");
+        return false;
     }
 
     @Override
     public boolean isTeleportPacket(Object packet) {
-        throw new UnsupportedOperationException("Not available on this platform");
+        return false;
     }
 
     @Override
     public boolean isNamedEntitySpawnPacket(Object packet) {
-        throw new UnsupportedOperationException("Not available on this platform");
+        return false;
     }
 
     @Override
     public boolean isMovePacket(Object packet) {
-        throw new UnsupportedOperationException("Not available on this platform");
+        return false;
     }
 
     @Override
     public boolean isLookPacket(Object packet) {
-        throw new UnsupportedOperationException("Not available on this platform");
+        return false;
     }
 
     @Override
     public int getTeleportEntityId(Object teleportPacket) {
-        throw new UnsupportedOperationException("Not available on this platform");
+        return 0;
     }
 
     @Override
     public int getMoveEntityId(Object movePacket) {
-        throw new UnsupportedOperationException("Not available on this platform");
+        return 0;
     }
 
     @Override
     public int getSpawnedPlayer(Object playerSpawnPacket) {
-        throw new UnsupportedOperationException("Not available on this platform");
+        return 0;
     }
 
     @Override
     public int[] getDestroyedEntities(Object destroyPacket) {
-        throw new UnsupportedOperationException("Not available on this platform");
+        return new int[0];
     }
 
     @Override
     public boolean isBundlePacket(Object packet) {
-        throw new UnsupportedOperationException("Not available on this platform");
+        return false;
     }
 
     @Override
     public Iterable<Object> getPackets(Object bundlePacket) {
-        throw new UnsupportedOperationException("Not available on this platform");
+        return Collections.emptyList();
     }
 }
