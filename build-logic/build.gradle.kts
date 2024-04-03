11f36e8a2205cbbc7e9dddd84398e9b4e87adcbf
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-04-03 13:05:57 -0400

    Update Loom and Gradle to Latest

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -5,5 +5,5 @@ plugins {
 dependencies {
     implementation("com.github.johnrengelman:shadow:8.1.1")
     implementation("io.freefair.gradle:lombok-plugin:8.0.1")
-    implementation("fabric-loom:fabric-loom.gradle.plugin:1.1-SNAPSHOT")
+    implementation("fabric-loom:fabric-loom.gradle.plugin:1.6-SNAPSHOT")
 }
