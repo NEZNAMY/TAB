cb394007a4fd3777dccf2161aec771c5ce36c129
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2024-04-23 17:25:00 +0200

    Update Fabric to 1.20.5

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 
 dependencies {
     api(projects.shared)
-    minecraft("com.mojang:minecraft:1.20.5-rc3")
+    minecraft("com.mojang:minecraft:1.20.5")
     mappings(loom.officialMojangMappings())
     modImplementation("me.lucko:fabric-permissions-api:0.2-SNAPSHOT")
     modImplementation("net.fabricmc:fabric-loader:0.15.9")
