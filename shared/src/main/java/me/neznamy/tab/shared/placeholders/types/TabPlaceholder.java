9ed277d80dca4ac5d770fcebb2b6df48b1bdfec1
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2024-06-20 13:44:16 +0200

    [Error fix] Make methods synchronized to avoid ConcurrentModificationException

--- shared/src/main/java/me/neznamy/tab/shared/placeholders/types/TabPlaceholder.java
+++ shared/src/main/java/me/neznamy/tab/shared/placeholders/types/TabPlaceholder.java
@@ -143,7 +143,7 @@ public abstract class TabPlaceholder implements Placeholder {
      * @param   parent
      *          parent placeholder using this placeholder in output
      */
-    public void addParent(@NonNull String parent) {
+    public synchronized void addParent(@NonNull String parent) {
         if (!parents.contains(parent)) parents.add(parent);
     }
 
@@ -154,7 +154,7 @@ public abstract class TabPlaceholder implements Placeholder {
      * @param   player
      *          Player to update placeholders for.
      */
-    public void updateParents(@NonNull TabPlayer player) {
+    public synchronized void updateParents(@NonNull TabPlayer player) {
         if (parents.isEmpty()) return;
         for (String id : parents) {
             TabPlaceholder pl = TAB.getInstance().getPlaceholderManager().getPlaceholder(id);
