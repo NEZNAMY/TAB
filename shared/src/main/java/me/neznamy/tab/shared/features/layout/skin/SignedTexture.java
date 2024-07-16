335f0a3d6f8bd2f86289c53ed5737104df3c2835
qdpy <quentin.dpy@gmail.com>
2024-07-16 03:52:26 +0200

    Add skin signed texture

---
+++ shared/src/main/java/me/neznamy/tab/shared/features/layout/skin/SignedTexture.java
@@ -0,0 +1,27 @@
+
+package me.neznamy.tab.shared.features.layout.skin;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.jetbrains.annotations.NotNull;
+import me.neznamy.tab.shared.config.file.ConfigurationFile;
+
+/**
+ * Skin source using raw texture;signature.
+ */
+public class SignedTexture extends SkinSource {
+
+    protected SignedTexture(@NotNull ConfigurationFile file) {
+        super(file, "signed_textures");
+    }
+
+    @Override
+    @NotNull
+    public List<String> download(@NotNull String textureBase64) {
+        String[] parts = textureBase64.split(";");
+        String base64 = parts[0];
+        String signature = parts[1];
+        return Arrays.asList(base64, signature);
+    }
+}
