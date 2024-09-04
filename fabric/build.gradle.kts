33cc4b2aad7139ed6c164122543ed41f496430dd
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2024-09-04 17:59:30 +0200

    [Fabric] Update to 24w36a

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 
 dependencies {
     api(projects.shared)
-    minecraft("com.mojang:minecraft:24w35a")
+    minecraft("com.mojang:minecraft:24w36a")
     mappings(loom.officialMojangMappings())
     modImplementation("me.lucko:fabric-permissions-api:0.2-SNAPSHOT")
     modImplementation("net.fabricmc:fabric-loader:0.15.9")
