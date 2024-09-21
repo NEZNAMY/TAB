7c5ef3aa8a91e2d0b18055e602426aece708cf5d
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2024-09-21 15:11:12 +0200

    [Bukkit] Bump dependency

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
     implementation(projects.shared)
     implementation("org.bstats:bstats-bukkit:3.0.1")
     compileOnly("org.purpurmc.purpur:purpur-api:1.16.5-R0.1-SNAPSHOT")
-    compileOnly("me.clip:placeholderapi:2.11.2")
+    compileOnly("me.clip:placeholderapi:2.11.6")
     compileOnly("com.mojang:authlib:1.5.25")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7") {
         exclude("org.bukkit", "bukkit")
