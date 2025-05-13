f4468261cfaf7dd277ac3c8eb1467837b69893cc
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2025-05-13 16:50:24 +0200

    [Fabric] Update to 25w20a

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -11,11 +11,11 @@ repositories {
     maven("https://maven.nucleoid.xyz/")
 }
 
-val minecraftVersion = "25w19a"
+val minecraftVersion = "25w20a"
 
 // Fabric API versions for each Minecraft version for easier backporting
 val fabricApiVersions = mapOf(
-    "25w19a" to "0.119.10+1.21.6",
+    "25w20a" to "0.119.10+1.21.6",
     "1.21.5" to "0.119.9+1.21.5",
     "1.21.4" to "0.119.2+1.21.4",
     "1.21.3" to "0.114.0+1.21.3",
