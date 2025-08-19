15664bc9fb707c74bcd175cd81018ce2f96dfee7
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2025-08-19 18:57:24 +0200

    Update loom to 1.11

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -5,5 +5,5 @@ plugins {
 dependencies {
     implementation("com.gradleup.shadow:shadow-gradle-plugin:8.3.6")
     implementation("io.freefair.gradle:lombok-plugin:8.13")
-    implementation("dev.architectury.loom:dev.architectury.loom.gradle.plugin:1.10-SNAPSHOT")
+    implementation("dev.architectury.loom:dev.architectury.loom.gradle.plugin:1.11-SNAPSHOT")
 }
