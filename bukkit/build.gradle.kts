cfb3d10a7d71c424c73778b1031e4c5c95df9134
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2025-08-26 20:16:29 +0200

    [Bukkit] Remove unused dependency

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -3,7 +3,6 @@ dependencies {
     implementation("org.bstats:bstats-bukkit:3.1.0")
     compileOnly("org.purpurmc.purpur:purpur-api:1.16.5-R0.1-SNAPSHOT")
     compileOnly("me.clip:placeholderapi:2.11.6")
-    compileOnly("com.mojang:authlib:1.5.25")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7") {
         exclude("org.bukkit", "bukkit")
     }
