4508fcf4b3646bdeee47b57b0ee537bf702154e7
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2025-09-14 11:01:44 +0200

    [Javadoc] Add some missing docs

--- bukkit/src/main/java/me/neznamy/tab/platforms/bukkit/provider/ImplementationProvider.java
+++ bukkit/src/main/java/me/neznamy/tab/platforms/bukkit/provider/ImplementationProvider.java
@@ -8,19 +8,54 @@ import me.neznamy.tab.shared.util.function.FunctionWithException;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * An interface to provide platform-specific implementations of various features in various Miniecraft versions.
+ */
 public interface ImplementationProvider {
 
+    /**
+     * Creates a new instance of scoreboard for the given player.
+     *
+     * @param   player
+     *          Player to create scoreboard for
+     * @return  Newly created scoreboard
+     */
     @NotNull
     Scoreboard newScoreboard(@NotNull BukkitTabPlayer player);
 
+    /**
+     * Creates a new instance of tab list for the given player.
+     *
+     * @param   player
+     *          Player to create tab list for
+     * @return  Newly created tab list
+     */
     @NotNull
     TabList newTabList(@NotNull BukkitTabPlayer player);
 
+    /**
+     * Returns component converter to be used for converting TAB components to NMS components.
+     *
+     * @return  Component converter
+     */
     @NotNull
     ComponentConverter getComponentConverter();
 
+    /**
+     * Returns a function that extracts Netty channel from the given player.
+     * If not supported in the version, returns {@code null}.
+     *
+     * @return  Channel extractor function or {@code null} if not supported
+     */
     @Nullable
     FunctionWithException<BukkitTabPlayer, Channel> getChannelFunction();
 
+    /**
+     * Returns ping of the given player.
+     *
+     * @param   player
+     *          Player to get ping of
+     * @return  Player's ping
+     */
     int getPing(@NotNull BukkitTabPlayer player);
 }
