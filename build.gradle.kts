91c6b8f08e12dc7bbde40ac42c34ea8829e7a1c5
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2025-10-09 19:29:26 +0200

    5.3.3-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 allprojects {
     group = "me.neznamy"
-    version = "5.3.2"
+    version = "5.3.3-SNAPSHOT"
     description = "An all-in-one solution that works"
 
     ext.set("id", "tab")
