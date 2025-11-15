1debc54db6bca35cde0c02f06edbaea0522f857d
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2025-11-15 12:36:34 +0100

    Update loom to 1.13-SNAPSHOT

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -5,5 +5,5 @@ plugins {
 dependencies {
     implementation("com.gradleup.shadow:shadow-gradle-plugin:9.2.2")
     implementation("io.freefair.gradle:lombok-plugin:9.0.0")
-    implementation("dev.architectury.loom:dev.architectury.loom.gradle.plugin:1.11-SNAPSHOT")
+    implementation("dev.architectury.loom:dev.architectury.loom.gradle.plugin:1.13-SNAPSHOT")
 }
