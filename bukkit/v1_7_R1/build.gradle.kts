7c30f55f3184e2033790fe0589f00e7e983283be
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2026-01-11 16:48:47 +0100

    [Bukkit] Remove support for several old MC versions

--- bukkit/v1_7_R1/build.gradle.kts
+++
@@ -1,4 +0,0 @@
-dependencies {
-    implementation(projects.bukkit)
-    compileOnly("org.bukkit:craftbukkit:1.7.2-R0.4-SNAPSHOT")
-}
--- bukkit/v1_8_R2/build.gradle.kts
+++
@@ -1,4 +0,0 @@
-dependencies {
-    implementation(projects.bukkit)
-    compileOnly("org.bukkit:craftbukkit:1.8.3-R0.1-SNAPSHOT")
-}
