c024b28a453b7fce05aa7f22c7f2651a9c9ebc2e
Crafting_tunbol <50541045+Crafting-tunbol@users.noreply.github.com>
2026-06-28 17:13:26 +0200

    Removed unused methods

--- shared/src/main/java/me/neznamy/tab/shared/placeholders/PlaceholderIdentifier.java
+++ shared/src/main/java/me/neznamy/tab/shared/placeholders/PlaceholderIdentifier.java
@@ -33,45 +33,4 @@ public final class PlaceholderIdentifier {
     public static boolean isRelational(@NotNull String identifier) {
         return identifier.startsWith("%rel_") || identifier.startsWith("<rel_");
     }
-
-    /**
-     * Checks if the given identifier is a server placeholder.
-     *
-     * @param   identifier
-     *          identifier to check
-     * @return  {@code true} if server placeholder, {@code false} otherwise
-     */
-    public static boolean isServer(@NotNull String identifier) {
-        return identifier.startsWith("%server_") || identifier.startsWith("<server_");
-    }
-
-    /**
-     * Converts the identifier into {@code %placeholder%} syntax.
-     *
-     * @param   identifier
-     *          identifier to convert
-     * @return  identifier in percent syntax
-     */
-    @NotNull
-    public static String toPercentSyntax(@NotNull String identifier) {
-        if (identifier.startsWith("<") && identifier.endsWith(">")) {
-            return "%" + identifier.substring(1, identifier.length() - 1) + "%";
-        }
-        return identifier;
-    }
-
-    /**
-     * Converts the identifier into MiniPlaceholders {@code <placeholder>} syntax.
-     *
-     * @param   identifier
-     *          identifier to convert
-     * @return  identifier in angle bracket syntax
-     */
-    @NotNull
-    public static String toAngleBracketSyntax(@NotNull String identifier) {
-        if (identifier.startsWith("%") && identifier.endsWith("%")) {
-            return "<" + identifier.substring(1, identifier.length() - 1) + ">";
-        }
-        return identifier;
-    }
 }
