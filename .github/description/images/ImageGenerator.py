2256e9ef77d1073cb5b0c8306ed45fd4724c6f27
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2026-08-09 08:25:00 +0200

    [Description] Update badges

--- .github/description/images/ImageGenerator.py
+++ .github/description/images/ImageGenerator.py
@@ -274,7 +274,7 @@ if __name__ == "__main__":
                  ]
     )
 
-    release = "6.1.1"
+    release = "6.1.2"
     download_image(f"https://img.shields.io/badge/Release-{release}-blue.svg", "Badge_Release.svg")
 
     java = "8+"
