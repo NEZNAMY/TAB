727c500b174f3abddd3ecf494e68fd4b2c8ac5c6
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2026-09-16 20:40:47 +0200

    [Bukkit] Update supported versions message

--- bukkit/src/main/java/me/neznamy/tab/platforms/bukkit/BukkitTAB.java
+++ bukkit/src/main/java/me/neznamy/tab/platforms/bukkit/BukkitTAB.java
@@ -28,7 +28,7 @@ public class BukkitTAB extends JavaPlugin {
                 Bukkit.getConsoleSender().sendMessage("§c[TAB] Use the other jar in the release instead.");
             } else {
                 Bukkit.getConsoleSender().sendMessage("§c[TAB] Your server version (" + Bukkit.getBukkitVersion() + ") is not supported.");
-                Bukkit.getConsoleSender().sendMessage("§c[TAB] This jar only supports 1.7.10, 1.8.8, 1.12.2, 1.16.5, 1.17.1 and 1.18.2 - 26.2");
+                Bukkit.getConsoleSender().sendMessage("§c[TAB] This jar only supports 1.7.10, 1.8.8, 1.12.2, 1.16.5, 1.17.1, 1.18.2 and 1.19.4 - 26.3");
                 Bukkit.getConsoleSender().sendMessage("§c[TAB] If you just updated to a new Minecraft version, check for TAB updates.");
                 Bukkit.getConsoleSender().sendMessage("§c[TAB] If you are using an unsupported 1.x version, use an older version of TAB (latest TAB 5.x supports all MC 1.x versions).");
                 Bukkit.getConsoleSender().sendMessage("§c[TAB] Thrown error message: " + e.getMessage());
